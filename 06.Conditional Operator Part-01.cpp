8c316609b9dad60c53625926b81524dda13d44b1
MD: Fazley Atif Maruf <106642848+MarufJob@users.noreply.github.com>
2024-01-13 23:30:26 +0600

    Add files via upload

---
+++ 06.Conditional Operator Part-01.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b,c,d;
+    a=5;
+    b=10;
+    (a<b) ? cout<<a<<endl:cout<<b<<endl;
+    
+    c=45,d=55;
+    (c>d) ? cout<<c<<endl:cout<<d<<endl;;
+
+    return 0;
+}
+
+
+/*
+Output: 5
+        55
+*/
