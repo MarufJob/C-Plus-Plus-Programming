ece7b2decb870b1c82d4a39837f3e14b03bb7054
MD: Fazley Atif Maruf <106642848+MarufJob@users.noreply.github.com>
2024-01-14 22:41:41 +0600

    Add files via upload

---
+++ 09.Conditional Statement Part-02 Even or Odd.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int a;
+    cin >>a;
+    
+    if (a%2==0)
+        cout <<"Even "<<a;
+    else
+        cout <<"Odd "<<a;
+    
+    return 0;
+}
+/*
+Input:99
+Output:Odd 99
+
+Input:44
+Output:Even 44
+*/
