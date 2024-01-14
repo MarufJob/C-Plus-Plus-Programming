5b9ce89ccc9d915e321601cd10b8a1acba566c46
MD: Fazley Atif Maruf <106642848+MarufJob@users.noreply.github.com>
2024-01-15 01:23:15 +0600

    Add files via upload

---
+++ 17.Create a Calculator using the switch Statement (switch ).cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char oper;
+    float num1, num2;
+    cout << "Enter an operator (+, -, *, /): ";
+    cin >> oper;
+    cout << "Enter two numbers: ";
+    cin >> num1 >> num2;
+
+    switch (oper) {
+        case '+':
+            cout << num1 << " + " << num2 << " = " << num1 + num2;
+            break;
+        case '-':
+            cout << num1 << " - " << num2 << " = " << num1 - num2;
+            break;
+        case '*':
+            cout << num1 << " * " << num2 << " = " << num1 * num2;
+            break;
+        case '/':
+            cout << num1 << " / " << num2 << " = " << num1 / num2;
+            break;
+        default:
+            // operator is doesn't match any case constant (+, -, *, /)
+            cout << "Error! The operator is not correct";
+            break;
+    }
+
+    return 0;
+}
+/*
+Input:  Enter an operator (+, -, *, /): +
+        Enter two numbers: 33 77
+Output: 33 + 77 = 110
+
+Input:  Enter an operator (+, -, *, /): *
+        Enter two numbers: 5 33
+Output: 5 * 33 = 165
+*/
