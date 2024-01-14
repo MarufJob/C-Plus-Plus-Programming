5d608bec4d6b26deda760a4cc1692051842d9427
MD: Fazley Atif Maruf <106642848+MarufJob@users.noreply.github.com>
2024-01-15 00:06:18 +0600

    Add files via upload

---
+++ 13.Display Numbers from 1 to N (while loop).cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int i = 1,n; 
+    cin>>n;
+
+    while (i <= n) {
+        cout << i << " ";
+        ++i;
+    }
+    
+    return 0;
+}
+/*
+Input:10
+Output:1 2 3 4 5 6 7 8 9 10 
+
+Input:15
+Output:1 2 3 4 5 6 7 8 9 10 11 12 13 14 15
+*/
