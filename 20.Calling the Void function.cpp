fb288345d96d4fa133da9611d1a59e95d8d4bc09
MD: Fazley Atif Maruf <106642848+MarufJob@users.noreply.github.com>
2024-01-16 23:33:37 +0600

    Add files via upload

---
+++ 20.Calling the Void function.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+// declaring a function
+// void not return any answer
+void greet() {
+    cout << "Hello there! This is a function.";
+}
+
+int main() {
+
+    // calling the function
+    greet();
+
+    return 0;
+}
+
+/*
+Output:Hello there! This is a function.
+*/
