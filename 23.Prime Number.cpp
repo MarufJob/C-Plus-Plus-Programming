c890e14cdc1590b70f45b59f138b3ec3b585131a
MD: Fazley Atif Maruf <106642848+MarufJob@users.noreply.github.com>
2024-01-16 23:59:01 +0600

    Add files via upload

---
+++ 23.Prime Number.cpp
@@ -0,0 +1,48 @@
+# include <iostream>
+using namespace std;
+
+
+int main()
+{
+    
+    int num, i, flag = 0;
+
+    cout << "Enter a positive integer enter to check: ";
+    cin >> num;
+    
+    if(num <= 0){
+        cout << "This is inbelite number.";
+        return 0;
+    }
+
+    for(i = 2; i <= num/2; ++i)
+    {
+        if(num % i == 0)
+        {
+            flag = 1; 
+            break;
+        }
+    }
+
+    if (flag == 1)
+    {
+        cout << num << " is not a prime number.";
+    }
+    else
+    {
+        cout << num << " is a prime number.";
+    }
+    
+    return 0;
+}
+
+/*
+Input:Enter a positive integer enter to check: 7
+Output:7 is a prime number.
+
+Input:Enter a positive integer enter to check: -5
+Output:This is inbelite number.
+
+Input:Enter a positive integer enter to check: 50
+Output:50 is not a prime number.
+*/
