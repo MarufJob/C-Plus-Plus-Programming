81184691aa2967ef794e54f07d865fa70f888475
MD: Fazley Atif Maruf <106642848+MarufJob@users.noreply.github.com>
2024-01-17 23:06:33 +0600

    Add files via upload

---
+++ 25.Function Overloading.cpp
@@ -0,0 +1,35 @@
+// Program to compute absolute value
+// Works for both int and float
+
+#include <iostream>
+using namespace std;
+
+// function with float type parameter
+float absolute(float var){
+    if (var < 0.0)
+        var = -var;
+    return var;
+}
+
+// function with int type parameter
+int absolute(int var) {
+     if (var < 0)
+         var = -var;
+    return var;
+}
+
+int main() {
+    
+    // call function with int type parameter
+    cout << "Absolute value of -5 = " << absolute(-5) << endl;
+
+    // call function with float type parameter
+    cout << "Absolute value of 5.5 = " << absolute(5.5f) << endl;
+    return 0;
+}
+
+
+/*
+Output: Absolute value of -5 = 5
+        Absolute value of 5.5 = 5.5
+*/
