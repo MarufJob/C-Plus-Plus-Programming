c30832fd6b89e6b73316f4220e2c5114609c7b12
MD: Fazley Atif Maruf <106642848+MarufJob@users.noreply.github.com>
2024-01-20 01:23:19 +0600

    Add files via upload

---
+++ 36.Passing 2D Array to a Function.cpp
@@ -0,0 +1,55 @@
+// C++ Program to display the elements of two
+// dimensional array by passing it to a function
+
+#include <iostream>
+using namespace std;
+
+// define a function 
+// pass a 2d array as a parameter
+void display(int n[][2]) {
+    cout << "Displaying Values: " << endl;
+    for (int i = 0; i < 3; ++i) {
+        for (int j = 0; j < 2; ++j) {
+            cout << "num[" << i << "][" << j << "]: " << n[i][j] << endl;
+        }
+    }
+    cout<<endl;
+}
+
+void rangeLoop(int (&arr)[3][2]){
+    for(const auto& raw:arr){
+        for(int element:raw)
+        {
+        cout<< element << " ";
+        }
+    }
+}
+
+int main() {
+
+    // initialize 2d array
+    int num[3][2] = {
+        {3, 4},
+        {9, 5},
+        {7, 1}
+    };
+
+    // call the function
+    // pass a 2d array as an argument
+    display(num);
+    rangeLoop(num);
+
+    return 0;
+}
+
+/*
+Output: Displaying Values: 
+        num[0][0]: 3
+        num[0][1]: 4
+        num[1][0]: 9
+        num[1][1]: 5
+        num[2][0]: 7
+        num[2][1]: 1
+        
+        3 4 9 5 7 1 
+*/
