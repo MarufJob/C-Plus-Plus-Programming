0e79e2f00fae6e8ad43ce0af3bfe21b7013c82e0
MD: Fazley Atif Maruf <106642848+MarufJob@users.noreply.github.com>
2024-01-21 23:12:59 +0600

    Add files via upload

---
+++ 41.Structure in C++.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+struct Person
+{
+    char name[50];
+    int age;
+    float salary;
+};
+
+int main()
+{
+    Person p1;
+    
+    cout << "Enter Full name: ";
+    cin.get(p1.name, 50);
+    cout << "Enter age: ";
+    cin >> p1.age;
+    cout << "Enter salary: ";
+    cin >> p1.salary;
+
+    cout << "\nDisplaying Information." << endl;
+    cout << "Name: " << p1.name << endl;
+    cout <<"Age: " << p1.age << endl;
+    cout << "Salary: " << p1.salary;
+
+    return 0;
+}
+/*
+Input:  Enter Full name: My name is Maruf.
+        Enter age: 25
+        Enter salary: 30000
+Output: Displaying Information.
+        Name: My name is Maruf.
+        Age: 25
+        Salary: 30000
+*/
