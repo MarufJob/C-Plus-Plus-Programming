e232a672206eedb9ddbade7da2149c02e1960cc5
MD: Fazley Atif Maruf <106642848+MarufJob@users.noreply.github.com>
2024-01-21 23:44:18 +0600

    Add files via upload

---
+++ 42.Structure and Function in C++.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+struct Person {
+    char name[50];
+    int age;
+    float salary;
+};
+
+void displayData(Person);   // Function declaration
+
+int main() {
+    Person p;
+
+    cout << "Enter Full name: ";
+    cin.get(p.name, 50);
+    cout << "Enter age: ";
+    cin >> p.age;
+    cout << "Enter salary: ";
+    cin >> p.salary;
+
+    // Function call with structure variable as an argument
+    displayData(p);
+
+    return 0;
+}
+
+void displayData(Person p) {
+    cout << "\nDisplaying Information." << endl;
+    cout << "Name: " << p.name << endl;
+    cout <<"Age: " << p.age << endl;
+    cout << "Salary: " << p.salary;
+}
+
+/*
+Input:  Enter Full name: My name is Maruf
+        Enter age: 25
+        Enter salary: 50000
+Output: Displaying Information.
+        Name: My name is Maruf
+        Age: 25
+        Salary: 50000
+*/
