d65497c44712a9ab11dbc0eca95936b04a89ddff
MD: Fazley Atif Maruf <106642848+MarufJob@users.noreply.github.com>
2024-01-22 22:51:25 +0600

    Update 47.Get the Value from the Address Using Pointers.cpp

--- 47.Get the Value from the Address Using Pointers.cpp
+++ 47.Get the Value from the Address Using Pointers.cpp
@@ -12,11 +12,16 @@ int main() {
     
     // access value pointed by pointVar
     cout << "value stored in that address= " << *pointVar << endl;   // Output: 5
-    cout << "address of var= "<<pointVar;
-
+    cout << "address of var= "<<pointVar <<endl;
+    cout << "value of var= "<< var <<endl;
+    cout << "address of var= "<< &var <<endl;
+    
     return 0;
 }
+
 /*
 Output: value stored in that address= 5
         address of var= 0x7ffd5585d244
+        value of var= 5
+        address of var= 0x7ffef1a20994
 */
