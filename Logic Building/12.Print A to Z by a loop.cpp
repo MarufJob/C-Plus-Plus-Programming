ab0ef72639229f2c31aaf43185d741f6960dc8b1
MD: Fazley Atif Maruf <106642848+MarufJob@users.noreply.github.com>
2024-01-30 23:51:20 +0600

    Add files via upload

---
+++ Logic Building/12.Print A to Z by a loop.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+int main() {
+    
+    char name;
+    
+    for(name='A'; name<='Z' ; name++)
+    {
+       
+    cout <<name<<endl;
+        
+    }
+    return 0;
+}
+/*
+
+Output: A
+        B
+        C
+        D
+        E
+        F
+        G
+        H
+        I
+        J
+        K
+        L
+        M
+        N
+        O
+        P
+        Q
+        R
+        S
+        T
+        U
+        V
+        W
+        X
+        Y
+        Z
+*/
