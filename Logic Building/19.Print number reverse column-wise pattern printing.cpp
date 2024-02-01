9da3dcb40df3b01bd95ea8881da62da9d2a51063
MD: Fazley Atif Maruf <106642848+MarufJob@users.noreply.github.com>
2024-02-01 23:45:19 +0600

    Add files via upload

---
+++ Logic Building/19.Print number reverse column-wise pattern printing.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int i,j,n;
+    
+    cout<<"Give number for Square = ";
+    cin >> n;
+    
+    for(i=1 ; i<=n ; i++){
+        for(j=n ; j>=1 ; j--){
+            cout <<j<<" ";
+        }
+        cout<<endl;
+    }
+    
+    return 0;
+}
+
+
+/*
+Input: Give number for Square = 5
+Output: 5 4 3 2 1 
+        5 4 3 2 1 
+        5 4 3 2 1 
+        5 4 3 2 1 
+        5 4 3 2 1 
+*/
