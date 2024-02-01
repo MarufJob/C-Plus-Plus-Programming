73e00fc739904fa0ec39209b670f3200326758e0
MD: Fazley Atif Maruf <106642848+MarufJob@users.noreply.github.com>
2024-02-01 23:25:12 +0600

    Add files via upload

---
+++ Logic Building/17.Print number row-wise pattern printing.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int i,j,n;
+    
+    cout<<"Give number for Square = ";
+    cin >> n;
+    
+    for(i=1 ; i<=n ; i++){
+        for(j=1 ; j<=n ; j++){
+            cout << i<<" ";
+        }
+        cout<<endl;
+    }
+    
+    return 0;
+}
+
+
+/*
+Input: Give number for Square = 5
+Output: 1 1 1 1 1 
+        2 2 2 2 2 
+        3 3 3 3 3 
+        4 4 4 4 4 
+        5 5 5 5 5 
+*/
