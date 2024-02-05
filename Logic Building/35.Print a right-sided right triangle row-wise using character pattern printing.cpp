8ee98895f044630f7d1d3ec2020389963e5413e7
MD: Fazley Atif Maruf <106642848+MarufJob@users.noreply.github.com>
2024-02-05 23:00:28 +0600

    Add files via upload

---
+++ Logic Building/35.Print a right-sided right triangle row-wise using character pattern printing.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int row,col,k,num;
+    
+    cout <<"Give number for pattern printing = ";
+    cin >> num;
+    
+    for(row=1 ; row<=num ; row++){
+        
+        // space print
+        for(col=1 ; col<=num-row ; col++){
+            cout <<"  ";
+        }
+        
+        // star print
+        for(col=1 ; col<=row ; col++){
+            char var='A'+col-1;
+            cout <<var<<" ";
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
+
+/*
+Input: Give number for pattern printing = 5
+Output:         A
+              A B
+            A B C
+          A B C D
+        A B C D E
+
+*/
