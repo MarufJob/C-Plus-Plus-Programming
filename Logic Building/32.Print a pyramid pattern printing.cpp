62bc6e6bb3621738e63e49e56092ca2a89d999e1
MD: Fazley Atif Maruf <106642848+MarufJob@users.noreply.github.com>
2024-02-05 22:29:08 +0600

    Update 32.Print a pyramid pattern printing.cpp

--- Logic Building/32.Print a pyramid pattern printing.cpp
+++ Logic Building/32.Print a pyramid pattern printing.cpp
@@ -1,9 +1,37 @@
 #include <iostream>
-
 using namespace std;
 
-int main()
-{
-    cout << "Hello world!" << endl;
+int main() {
+    
+    int i,j,k,num;
+    
+    cout <<"Give number for pattern printing = ";
+    cin >> num;
+    
+    for(i=1 ; i<=num ; i++){
+        
+        // space print
+        for(j=1 ; j<=num-i ; j++){
+            cout <<" ";
+        }
+        
+        // star print
+        for(k=1 ; k<=i ; k++){
+            cout <<"* ";
+        }
+        cout<<endl;
+    }
+
     return 0;
 }
+
+
+/*
+Input: Give number for pattern printing = 5
+Output:     *
+           * *
+          * * *
+         * * * *
+        * * * * *
+
+*/
