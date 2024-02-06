81908ea1a6919154dafe4463b82e7ca3577dbc9b
MD: Fazley Atif Maruf <106642848+MarufJob@users.noreply.github.com>
2024-02-06 22:29:23 +0600

    Update 37.Print a pyramid advanced pattern printing.cpp

--- Logic Building/37.Print a pyramid advanced pattern printing.cpp
+++ Logic Building/37.Print a pyramid advanced pattern printing.cpp
@@ -16,13 +16,17 @@ int main() {
         }
 
         // star print
-        for(col=1 ; col<=row ; col++){
+         for(col=1 ; col<=2*row-1 ; col++){
             cout <<"* ";
         }
+
+        // for(col=1 ; col<=row ; col++){
+        //     cout <<"* ";
+        // }
         
-        for(col=1 ; col<=row-1 ; col++){
-            cout <<"* ";
-        }
+        // for(col=1 ; col<=row-1 ; col++){
+        //     cout <<"* ";
+        // }
         
         cout<<endl;
     }
