7d017302db78c4f56d1e290ace89a9f7e8f5b2bf
MD: Fazley Atif Maruf <106642848+MarufJob@users.noreply.github.com>
2024-02-06 23:59:42 +0600

    Add files via upload

---
+++ Logic Building/39.Print an inverted pyramid advanced pattern printing.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int row,col,num;
+    
+    cout <<"Give number for pattern printing = ";
+    cin >> num;
+    
+    for(row=1 ; row<=num ; row++){
+
+        // space print
+        for(col=1 ; col<=row-1 ; col++){
+            cout <<"  ";
+        }
+
+        // star print
+         for(col=1 ; col<=2*(num-row)+1 ; col++){
+            cout <<"* ";
+        }
+
+        cout<<endl;
+    }
+
+    return 0;
+}
+
+
+/*
+Input: Give number for pattern printing = 5
+Output:     * * * * * * * * * 
+              * * * * * * * 
+                * * * * * 
+                  * * * 
+                    * 
+*/
