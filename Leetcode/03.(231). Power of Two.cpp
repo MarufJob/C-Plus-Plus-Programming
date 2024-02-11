7f6b8f0da9a2b645c9b418a3c15570a972712dbd
MD: Fazley Atif Maruf <106642848+MarufJob@users.noreply.github.com>
2024-02-12 00:40:46 +0600

    Update 03.(231). Power of Two.cpp

--- Leetcode/03.(231). Power of Two.cpp
+++ Leetcode/03.(231). Power of Two.cpp
@@ -1,9 +1,21 @@
-#include <iostream>
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        int rem=0;
 
-using namespace std;
+        if(n<=0)
+        return false;
 
-int main()
-{
-    cout << "Hello world!" << endl;
-    return 0;
-}
+        while(n!=1){
+            rem=n%2;
+            if(rem==1)
+            {
+                //cout<<"";
+                return false;
+            }
+            n=n/2;
+        }
+        // At this point, x is 1, indicating x is a power of two
+        return true;
+    }
+};
