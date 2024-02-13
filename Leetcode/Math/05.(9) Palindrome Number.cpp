ad75be72c64a20dcf3ec2ac1c43dbb997cbae9f8
MD: Fazley Atif Maruf <106642848+MarufJob@users.noreply.github.com>
2024-02-13 17:08:56 +0600

    Update 05.(9) Palindrome Number.cpp

--- Leetcode/Math/05.(9) Palindrome Number.cpp
+++ Leetcode/Math/05.(9) Palindrome Number.cpp
@@ -1,1 +1,25 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
 
+        if(x<0)
+        return false;
+        int num=x, rem, ans=0;
+
+        while(x!=0){
+            rem=x%10;
+            x=x/10;
+            //for integer overflow
+            if(ans>INT_MAX/10)
+            return false;
+
+            ans=ans*10+rem;
+        }
+            if(ans==num){
+                return true;
+            }
+
+        return false;
+        
+    }
+};
