5b1e7e68323179f78ba8992fe409a7544398e193
MD: Fazley Atif Maruf <106642848+MarufJob@users.noreply.github.com>
2024-02-18 22:31:49 +0600

    Update 04.Second Largest.cpp

--- Geeksforgeeks/04.Second Largest.cpp
+++ Geeksforgeeks/04.Second Largest.cpp
@@ -69,3 +69,39 @@ Explanation: The largest element of
 the array is 10 and the second 
 largest element is 5.
 */
+
+/*
+//*****Easy*****
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	// Function returns the second
+	// largest elements
+	int print2largest(int arr[], int n) {
+	    // code here
+	    int ans = INT_MIN, lar= INT_MIN;
+	    //Largest element
+	    for(int i=0; i<n ; i++){
+	        
+	        if(arr[i]>lar){
+	            ans=lar;
+	            lar=arr[i];
+	        }
+	        
+	        if(ans < arr[i] && arr[i] < lar)
+	        ans = arr[i];
+	        
+	    }
+	    if (ans==INT_MIN)
+	    return -1;
+	    
+	    return ans;
+	}
+};
+*/
