ef1df32d636fce5578b75c9822154dc08961649e
MD: Fazley Atif Maruf <106642848+MarufJob@users.noreply.github.com>
2024-02-26 10:31:09 +0600

    Update ০১.জোড়-বিজোড় ১.cpp

--- ৫২ টি প্রোগ্রামিং সমস্যা ও সমাধান/০১.জোড়-বিজোড় ১.cpp
+++ ৫২ টি প্রোগ্রামিং সমস্যা ও সমাধান/০১.জোড়-বিজোড় ১.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
 
+using namespace std;
+
+int main()
+{
+    int i,T,num;
+
+    cout<<"How many number you taking = ";
+    cin>>T;
+
+    for(i=1 ; i<=T ; i++)
+    {
+        cout<<"Give a number = ";
+        cin>>num;
+
+        if(num%2==0)
+        {
+            cout<<"even"<<endl;
+        }
+        else
+        {
+            cout<<"odd"<<endl;
+        }
+    }
+
+    return 0;
+}
