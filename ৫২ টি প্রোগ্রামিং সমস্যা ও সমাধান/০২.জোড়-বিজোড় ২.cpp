54d27c71aab372cac455192ae39203e232c1ecfd
MD: Fazley Atif Maruf <106642848+MarufJob@users.noreply.github.com>
2024-02-27 10:21:46 +0600

    Add files via upload

---
+++ ৫২ টি প্রোগ্রামিং সমস্যা ও সমাধান/০২.জোড়-বিজোড় ২.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i,T;
+    char num[101];
+
+
+    cout<<num.leanth;
+
+
+/*
+    cout<<"How many number you taking = ";
+    cin>>T;
+
+    for(i=1 ; i<=T ; i++)
+    {
+        cout<<"Give a number = ";
+        cin>>num;
+
+        if(num%'2'=='0')
+        {
+            cout<<"even"<<endl;
+        }
+        else
+        {
+            cout<<"odd"<<endl;
+        }
+    }
+    */
+
+    return 0;
+}
