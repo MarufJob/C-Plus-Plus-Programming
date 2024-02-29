a3ed8a47c77785b760f4164f85a56bd3f4639734
MD: Fazley Atif Maruf <106642848+MarufJob@users.noreply.github.com>
2024-02-29 19:47:43 +0600

    Add files via upload

---
+++ ৫২ টি প্রোগ্রামিং সমস্যা ও সমাধান/০৬.যোগফল নির্ণয়.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i,j,k,T,num,m,n,sum;
+
+    cout<<"How many number you taking = ";
+    cin>>T;
+
+    for(i=1 ; i<=T ; i++)
+    {
+        sum=0;
+        cout<<"Give a number = ";
+        cin>>num;
+
+       for(j=1 ; j<=5 ; j++)
+       {
+           m=num%10;
+           num=num/10;
+           if(j==1 || j==5)
+           {
+               sum=sum+m;
+           }
+
+
+
+
+            /*for(k=1 ; k<=num ; k++)
+            {
+              cout<<"*";
+            }*/
+            //cout<<endl;
+       }
+       cout<<"Sum = "<<sum;
+       cout<<endl;
+    }
+
+    return 0;
+}
+
+
+/*
+        How many number you taking = 3
+Input : Give a number = 100
+Output :even
+
+Input : Give a number = 0
+Output :even
+
+Input : Give a number = 1111
+Output :odd
+*/
+
+/*
+How many number you taking = 5
+Input :Give a number = 12345
+Output :Sum = 6
+Input :Give a number = 56789
+Output :Sum = 14
+Input :Give a number = 14310
+Output :Sum = 1
+Input :Give a number = 10062
+Output :Sum = 3
+Input :Give a number = 96587
+Output :Sum = 16
+*/
