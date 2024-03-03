61d577b2bde91f3e78b79567cf49e685f7f0c9a9
MD: Fazley Atif Maruf <106642848+MarufJob@users.noreply.github.com>
2024-03-03 09:49:30 +0600

    Add files via upload

---
+++ ৫২ টি প্রোগ্রামিং সমস্যা ও সমাধান/১১.গৌনিক বা ফ্যাক্টরিয়াল.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int T,num1,num2=1,temp,i,j,nth;
+
+    cout<<"How many number you taking = ";
+    cin>>T;
+
+    for(i=1 ; i<=T ; i++)
+    {
+        temp=1;
+        cout<<"Give a number = ";
+        cin>>nth;
+
+        for(j=1 ; j<=nth ; j++)
+        {
+
+            temp = j*temp;
+        }
+
+       cout<<temp<<endl;
+    }
+
+    return 0;
+}
+
+
+/*
+        How many number you taking = 3
+Input : Give a number = 100
+Output :even
+
+Input : Give a number = 0
+Output :even
+
+Input : Give a number = 1111
+Output :odd
+*/
+
+/*How many number you taking = 5
+Give a number = 12345
+Sum = 6
+Give a number = 56789
+Sum = 14
+Give a number = 14310
+Sum = 1
+Give a number = 10062
+Sum = 3
+Give a number = 96587
+Sum = 16
+*/
