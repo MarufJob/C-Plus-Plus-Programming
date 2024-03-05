74f7bb1b7d2d2d8eadaeef6e8d0538dc2efc2bac
MD: Fazley Atif Maruf <106642848+MarufJob@users.noreply.github.com>
2024-03-05 22:33:07 +0600

    Update ০৮.ছোট থেকে বড়.cpp

--- ৫২ টি প্রোগ্রামিং সমস্যা ও সমাধান/০৮.ছোট থেকে বড়.cpp
+++ ৫২ টি প্রোগ্রামিং সমস্যা ও সমাধান/০৮.ছোট থেকে বড়.cpp
@@ -4,52 +4,54 @@ using namespace std;
 
 int main()
 {
-    int T,num1,num2,temp,kase;
+    int T,num1,num2,num3,temp,kase;
 
     cout<<"How many number you taking = ";
     cin>>T;
 
     for(kase=1 ; kase<=T ; kase++)
     {
-        //sum=0;
-        cout<<"Give a number = ";
-        cin>>num1>>num1;
+        cout<<"Give three number = ";
+        cin>>num1>>num2>>num3;
         
-        if(num1>num1)
+        if(num1>num2 )
         {
             temp = num1;
             num1 = num2;
             num2 = temp;
+            //cout<<"kase "<<kase<<": "<<num1<<" "<<num2<<" "<<num3<<endl;
         }
-
-       cout<<"Case "<<kase<<" "<<num1<<" "<<num2;
-       cout<<endl;
+        
+        if(num2>num3 )
+        {
+            temp = num2;
+            num2 = num3;
+            num3 = temp;
+            //cout<<"kase "<<kase<<": "<<num1<<" "<<num2<<" "<<num3<<endl;
+        }
+        
+        if(num1>num2 )
+        {
+            temp = num1;
+            num1 = num2;
+            num2 = temp;
+            //cout<<"kase "<<kase<<": "<<num1<<" "<<num2<<" "<<num3<<endl<<endl<<endl;
+        }
+       
+       cout<<"Case "<<kase<<": "<<num1<<" "<<num2<<" "<<num3<<endl;
+    }
       
     return 0;
 }
 
-
 /*
-        How many number you taking = 3
-Input : Give a number = 100
-Output :even
+How many number you taking = 3
+Input : Give three number = 3 2 1
+Output :Case 1: 1 2 3
 
-Input : Give a number = 0
-Output :even
+Input : Give three number = 1 2 3
+Output :Case 2: 1 2 3
 
-Input : Give a number = 1111
-Output :odd
-*/
-
-/*How many number you taking = 5
-Give a number = 12345
-Sum = 6
-Give a number = 56789
-Sum = 14
-Give a number = 14310
-Sum = 1
-Give a number = 10062
-Sum = 3
-Give a number = 96587
-Sum = 16
+Input : Give three number = 10 5 6
+Output :Case 3: 5 6 10
 */
