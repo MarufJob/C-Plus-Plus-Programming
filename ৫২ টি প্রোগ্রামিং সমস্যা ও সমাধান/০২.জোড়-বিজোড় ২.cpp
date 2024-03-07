b939bb7b44a9d33cd0bf33121d644012cc86f45c
MD: Fazley Atif Maruf <106642848+MarufJob@users.noreply.github.com>
2024-03-07 22:55:26 +0600

    Update ০২.জোড়-বিজোড় ২.cpp

--- ৫২ টি প্রোগ্রামিং সমস্যা ও সমাধান/০২.জোড়-বিজোড় ২.cpp
+++ ৫২ টি প্রোগ্রামিং সমস্যা ও সমাধান/০২.জোড়-বিজোড় ২.cpp
@@ -1,4 +1,49 @@
 #include <iostream>
+#include <cstring>
+using namespace std;
+
+int main()
+{
+    int i,T;
+    char num[100];
+    //char num1;
+
+    cout<<"How many number you taking = ";
+    cin>>T;
+
+    for(i=1 ; i<=T ; i++)
+    {
+        cout<<"Give a number = ";
+        cin>>num;
+
+        //num1 = num.at(num.length() - 1);
+        int length = strlen(num);
+        int last_digit = num[length-1];
+
+        if(last_digit=='0'||last_digit=='2'||last_digit=='4'||last_digit=='6'||last_digit=='8')
+        {
+            cout<<"even"<<endl;
+        }
+        else
+        {
+            cout<<"odd"<<endl;
+        }
+    }
+    
+    return 0;
+}
+
+/*
+How many number you taking = 3
+Input : Give a number = 100
+Output: even
+Input : Give a number = 0
+Output: even
+Input : Give a number = 1111
+Output: odd
+*/
+/*
+#include <iostream>
 using namespace std;
 
 int main()
@@ -29,13 +74,4 @@ int main()
     
     return 0;
 }
-
-/*
-How many number you taking = 3
-Input : Give a number = 100
-Output: even
-Input : Give a number = 0
-Output: even
-Input : Give a number = 1111
-Output: odd
 */
