e71309f913322e085e7ce1aa3b61344e485615c3
MD: Fazley Atif Maruf <106642848+MarufJob@users.noreply.github.com>
2024-03-10 23:41:29 +0600

    Update ০৭.সংখ্যা গণনা.cpp

--- ৫২ টি প্রোগ্রামিং সমস্যা ও সমাধান/০৭.সংখ্যা গণনা.cpp
+++ ৫২ টি প্রোগ্রামিং সমস্যা ও সমাধান/০৭.সংখ্যা গণনা.cpp
@@ -1,9 +1,31 @@
 #include <iostream>
-
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    int i,T;
+    string num;
+    int num1;
+
+    cout<<"How many number you taking = ";
+    cin>>T;
+
+    for(i=1 ; i<=T ; i++)
+    {
+        cout<<"Give a number = ";
+        cin>>num;
+
+        num1 = num.length();
+
+        // if(num1=='0'||num1=='2'||num1=='4'||num1=='6'||num1=='8')
+        // {
+        //     cout<<"even"<<endl;
+        // }
+        // else
+        // {
+        //     cout<<"odd"<<endl;
+        // }
+    }
+    
     return 0;
 }
