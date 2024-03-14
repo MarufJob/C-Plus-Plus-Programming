4532b6dc8e1bf09b6dcdce5c9ed9d14867a2144d
MD: Fazley Atif Maruf <106642848+MarufJob@users.noreply.github.com>
2024-03-14 22:46:14 +0600

    Update ১০.রান রেট-১.cpp

--- ৫২ টি প্রোগ্রামিং সমস্যা ও সমাধান/১০.রান রেট-১.cpp
+++ ৫২ টি প্রোগ্রামিং সমস্যা ও সমাধান/১০.রান রেট-১.cpp
@@ -12,32 +12,30 @@ int main()
 
     for(i=1 ; i<=T ; i++)
     {
+        CurRunrate=0;
+        NeedRunrate=0;
+        
         cout<<"Give three number = ";
         cin>>num1>>num2>>num3;
         
         Odone=300-num3;
-        CurRunrate=(num2/Odone)*6;
-        NeedRunrate=((num1-num2+1)/num3)*6;
+        CurRunrate=(num2*1.0/Odone)*6;
+        NeedRunrate=((num1*1.0-num2+1)/num3)*6;
         
-        cout<<setprecision(3)<<CurRunrate<<" "<<NeedRunrate<<endl;
-        
-        
-
-        
+        cout<<setprecision(3)<<CurRunrate<<" "<<setprecision(3)<<NeedRunrate<<endl;
     }
 
     return 0;
 }
 
-
 /*
-How many number you taking = 3
-Input : Give a number = 16
-Output :Yes
-
-Input : Give a number = 18
-Output :No
-
-Input : Give a number = 196
-Output :Yes
+How many number you taking = 4
+Input :Give three number = 300 294 6
+Output:6 7
+Input :Give three number = 200 100 100
+Output:3 6.06
+Input :Give three number = 333 250 40
+Output:5.77 12.6
+Input :Give three number = 118 100 180
+Output:5 0.633
 */
