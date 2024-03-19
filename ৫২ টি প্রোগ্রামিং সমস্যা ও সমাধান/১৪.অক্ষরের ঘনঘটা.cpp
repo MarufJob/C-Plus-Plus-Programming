0382a49c921966d95cedaa786fee22958343d805
MD: Fazley Atif Maruf <106642848+MarufJob@users.noreply.github.com>
2024-03-19 22:24:15 +0600

    Add files via upload

---
+++ ৫২ টি প্রোগ্রামিং সমস্যা ও সমাধান/১৪.অক্ষরের ঘনঘটা.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i, T;
+    char str[10000];
+    char c;
+
+    cout << "How many input are you taking = ";
+    cin >> T;
+    cin.ignore(); // Consume the newline character
+
+    for (i = 1; i <= T; i++)
+    {
+        int num = 0;
+        cout << "Give a string: ";
+        cin.get(str, 100);
+        cin.ignore(); // Consume the newline character after inputting the string
+        cin >> c;
+        cin.ignore();
+
+        for (int i = 0; str[i] != '\0'; i++)
+        {
+            if (str[i] == c)
+            {
+                num++;
+            }
+        }
+        if(num>0)
+        cout <<"Occurrence of " <<"'"<<c<<"'"<<" in "<<"'"<<str<<"' = "<<num<< endl;
+        else
+        cout<<"'"<<c<<"'"<<" is not present"<<endl;
+    }
+    return 0;
+}
+
+
+/*
+How many input are you taking = 2
+Input: Give a string: hello world
+       l
+Output:Occurrence of 'l' in 'hello world' = 3
+Input: Give a string: hello world
+       a
+Output:'a' is not present
+*/
