42e80076a172351b3492b0be75381dc16bbb8415
MD: Fazley Atif Maruf <106642848+MarufJob@users.noreply.github.com>
2024-03-25 23:46:27 +0600

    Add files via upload

---
+++ 39.(ii)String variables in C++ .cpp.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+void display(char *);
+void display(string);
+
+int main()
+{
+    char str[100];
+    string str1;
+
+    cout << "Enter a string: ";
+    cin.get(str, 100, '\n');
+    cin.ignore(); // Consume the newline character after inputting the string
+
+    cout << "Enter another string: ";
+    getline(cin, str1);
+
+    display(str);
+    display(str1);
+    return 0;
+}
+
+void display(char stringVar[])
+{
+    cout << "Entered char array is: " << stringVar << endl;
+}
+
+void display(string stringVar)
+{
+    cout << "Entered string is: " << stringVar << endl;
+}
+
+
+
+/*
+Input: Enter a string: My name is Md.Fazley Atif Maruf
+       Enter another string: I am a male
+Output:Entered char array is: My name is Md.Fazley Atif Maruf
+       Entered string is: I am a male
+*/
