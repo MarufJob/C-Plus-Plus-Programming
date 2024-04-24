cf734c199bd75bbda51a7dc07fbba11e388f237d
MD: Fazley Atif Maruf <106642848+MarufJob@users.noreply.github.com>
2024-04-24 23:13:55 +0600

    Add files via upload

---
+++ ৫২ টি প্রোগ্রামিং সমস্যা ও সমাধান/২১.উল্টে দেখা.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main()
+{
+    int i, T;
+    //char str[1000];
+
+    cout << "How many input are you taking = ";
+    cin >> T;
+    cin.ignore(); // Consume the newline character
+
+    for (i = 1; i <= T; i++)
+    {
+        int s_len=0;
+        cout << "Give a string: ";
+        char str[1000]={};
+        cin.get(str, 1000);
+        cin.ignore(); // Consume the newline character after inputting the string
+        
+        s_len=strlen(str);
+        
+        
+            for (int j = s_len-1; j >= 0; j--)
+            {
+                cout<<str[j];
+                // if(j==0)
+                // break;
+                
+                
+                
+                // //cout <<"line j = "<<j<<endl;
+                // if (str[j] == ' '|| str[j+1] == '\0')
+                // {
+                //     if(str[j+1] == '\0')
+                //     j++;
+                //   for(int k=j-1 ; k!=' ' ; k--)
+                //     {
+                //         //cout <<"line k = "<<k<<endl;
+                //         if(k<0||str[k]==' ')
+                //         {
+                //             if(str[j+1] == '\0')
+                //             break;
+                //             cout<<' ';
+                //             break;
+                //         }
+                //          cout <<str[k];
+                //         //cout<<endl;
+                //     }
+                // }
+            }
+            cout<<endl;
+    }
+    return 0;
+}
+
+
+/*
+How many input are you taking = 4
+Input: Give a string: This is a test
+Output:sihT si a tset
+Input: Give a string: Hello World
+Output:olleH dlroW
+Input: Give a string: don't underestimate the
+Output:t'nod etamitserednu eht
+Input: Give a string: power of a girl
+Output:rewop fo a lrig
+*/
