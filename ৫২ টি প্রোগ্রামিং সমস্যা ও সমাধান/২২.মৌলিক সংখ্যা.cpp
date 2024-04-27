a5b66e07f0beb47fea826e60e8a624204f6daa4f
MD: Fazley Atif Maruf <106642848+MarufJob@users.noreply.github.com>
2024-04-27 20:50:37 +0600

    Update ২২.মৌলিক সংখ্যা.cpp

--- ৫২ টি প্রোগ্রামিং সমস্যা ও সমাধান/২২.মৌলিক সংখ্যা.cpp
+++ ৫২ টি প্রোগ্রামিং সমস্যা ও সমাধান/২২.মৌলিক সংখ্যা.cpp
@@ -20,3 +20,40 @@ int main() {
 
     return 0;
 }
+
+/*
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i,j,num;
+    
+    cout<<"Give a number = ";
+    cin>>num;
+    
+    if(num<2)
+    {
+        cout<<num<<"is not prime";
+    }
+    
+        for(j=2 ; j<num ; j++)
+        {
+            if(i%j==0)
+            cout<<"Not Prime";
+            break;
+            
+            // if(i%j!=0 || j==i/2)
+            // cout<<i<<" ";
+            
+            // else
+            // {
+            // cout<<i<<" ";
+            // break;
+            // }
+        }
+        //cout<<"Prime";
+    // }
+
+    return 0;
+}
+*/
