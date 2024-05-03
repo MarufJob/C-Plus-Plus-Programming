eee54708f55e022a4f3f121f591e95c8762a44bf
MD: Fazley Atif Maruf <106642848+MarufJob@users.noreply.github.com>
2024-05-03 19:52:37 +0600

    Update ২২.মৌলিক সংখ্যা.cpp

--- ৫২ টি প্রোগ্রামিং সমস্যা ও সমাধান/২২.মৌলিক সংখ্যা.cpp
+++ ৫২ টি প্রোগ্রামিং সমস্যা ও সমাধান/২২.মৌলিক সংখ্যা.cpp
@@ -1,59 +1,71 @@
 #include <iostream>
+#include <cstring>
+
 using namespace std;
 
-int main() {
-    int i,j,num;
+int main() 
+{
     
-    cin>>num;
+    int t,i,j,k,l,num,count;
     
-    for(i=1 ; i<=num ; i++)
+    cout<<"How many input are you taking = ";
+    cin>>t;
+    cin.ignore();// Consume the newline character after inputting the string
+    
+    for(i=1 ; i<=t ; i++)
     {
-        for(j=2 ; j<=i/2 ; j++)
-        {
-            if(i%j==0)
-            break;
-            
-            else
-            cout<<i<<" ";
-        }
-    }
+        //int i,j,k,mid,num;
+        count = 0;
 
+        cout<<"Strat = ";
+        cin>>l;
+        cout<<"end = ";
+        cin>>num;
+
+
+        for(k=l ; k<=num ; k++)
+        {
+            int countNum = 0,num2 = 0;
+    
+            for(j=2 ; j*j<=k ; j++)
+            {
+                if(k%j==0)
+                {
+                    countNum = 1;
+                    break;
+                }
+                //num2++;
+            }
+    
+            if(countNum == 0 && k >1)
+                count++;
+                //cout<<k<<" -> "<<num2<<endl;
+                //cout<<k<<" ";
+        }
+        //cout<<"T = "<<t<<"  "<<count<<endl;
+        cout<<count<<endl;
+        //cout<<endl;
+    }
+    
     return 0;
 }
+
 
 /*
-#include <iostream>
-using namespace std;
-
-int main() {
-    int i,j,num;
-    
-    cout<<"Give a number = ";
-    cin>>num;
-    
-    if(num<2)
-    {
-        cout<<num<<"is not prime";
-    }
-    
-        for(j=2 ; j<num ; j++)
-        {
-            if(i%j==0)
-            cout<<"Not Prime";
-            break;
-            
-            // if(i%j!=0 || j==i/2)
-            // cout<<i<<" ";
-            
-            // else
-            // {
-            // cout<<i<<" ";
-            // break;
-            // }
-        }
-        //cout<<"Prime";
-    // }
-
-    return 0;
-}
+How many input are you taking = 5
+Input : Strat = 1
+        end = 5
+Output: 3
+Input : Strat = 25
+        end = 50
+Output: 6
+Input : Strat = 12
+        end = 27
+Output: 4
+Input : Strat = 94635
+        end = 99735
+Output: 443
+Input : Strat = 91752
+        end = 95402
+Output: 332
 */
