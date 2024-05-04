673c7a3eb0812c86b364c4841cde9666e38fcfa8
MD: Fazley Atif Maruf <106642848+MarufJob@users.noreply.github.com>
2024-05-04 22:29:08 +0600

    Update ২২.মৌলিক সংখ্যা.cpp

--- ৫২ টি প্রোগ্রামিং সমস্যা ও সমাধান/২২.মৌলিক সংখ্যা.cpp
+++ ৫২ টি প্রোগ্রামিং সমস্যা ও সমাধান/২২.মৌলিক সংখ্যা.cpp
@@ -69,3 +69,46 @@ Input : Strat = 91752
         end = 95402
 Output: 332
 */
+
+
+
+
+/*
+
+#include <iostream>
+#include<math.h>
+#define size 100001
+char ara[size];
+
+
+void sieve()
+{
+    int i,j,root;
+    for(i=2;i<size;i++)
+    {
+        ara[i]=1;
+    }
+    root = sqrt(size);
+    for(i=2;i<=root;i++)
+    {
+        if(ara[i==1])
+        {
+            for(j=2; i*j<=size;j++)
+            {
+                ara[i*j]=0;   
+            }
+        }
+    }
+}
+
+
+
+
+int main() {
+    // Write C++ code here
+    std::cout << "Try programiz.pro";
+
+    return 0;
+}
+
+*/
