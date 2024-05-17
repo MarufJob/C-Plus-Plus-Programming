2f0ea5f89bc8dfc9deecd4c4405cca1f33150874
MD: Fazley Atif Maruf <106642848+MarufJob@users.noreply.github.com>
2024-05-17 19:40:11 +0600

    Update ১৮.স্বরবর্ণ-ব্যঞ্জনবর্ণ.cpp

--- ৫২ টি প্রোগ্রামিং সমস্যা ও সমাধান/১৮.স্বরবর্ণ-ব্যঞ্জনবর্ণ.cpp
+++ ৫২ টি প্রোগ্রামিং সমস্যা ও সমাধান/১৮.স্বরবর্ণ-ব্যঞ্জনবর্ণ.cpp
@@ -57,3 +57,49 @@ Input: Give a string: his mind was blown
 Output: iiao
         hsmndwsblwn
 */
+
+
+
+/*
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i, T;
+    char str[1001];
+
+    cout << "How many input are you taking = ";
+    cin >> T;
+    cin.ignore(); // Consume the newline character
+
+    for (i = 1; i <= T; i++)
+    {
+        cout << "Give a string: ";
+        cin.get(str, 10001);
+        cin.ignore(); // Consume the newline character after inputting the string
+        char consonant[9000] = {};
+
+        
+            for (int j = 0,k = 0; str[j] != '\0'; j++)
+            {
+                if (str[j] == 'a' || str[j] == 'e' || str[j] == 'i' || str[j] == 'o' || str[j] == 'u')
+                    {
+                         cout<<str[j];
+                    }
+                else if(str[j]!=' ')
+                
+                    {
+                        consonant[k]=str[j];
+                        k++;
+                    }
+                
+            }
+            cout<<endl;
+            cout<<consonant<<endl;
+    }
+    return 0;
+}
+
+*/
