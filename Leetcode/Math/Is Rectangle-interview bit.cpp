a33ca29e1d1b103c56bb7f67f12d9158083b65d8
MD: Fazley Atif Maruf <106642848+MarufJob@users.noreply.github.com>
2024-05-20 05:51:31 +0600

    Add files via upload

---
+++ Leetcode/Math/Is Rectangle-interview bit.cpp
@@ -0,0 +1,47 @@
+/**
+ * @input A : Integer
+ * @input B : Integer
+ * @input C : Integer
+ * @input D : Integer
+ * 
+ * @Output Integer
+ */
+int solve(int A, int B, int C, int D) {
+    if(A==B && C==D)
+        {
+            return 1;
+        }
+    
+    else if(A==C && B==D)
+        {
+            return 1;
+        }
+    
+    else if(A==D && B==C)
+        {
+            return 1;
+        }
+    
+    else
+        {
+            return 0;
+        }
+}
+
+
+
+/*
+
+int solve(int A, int B, int C, int D) {
+    if((A==B && C==D) || (A==C && B==D) || (A==D && B==C))
+        {
+            return 1;
+        }
+    
+    else
+        {
+            return 0;
+        }
+}
+
+*/
