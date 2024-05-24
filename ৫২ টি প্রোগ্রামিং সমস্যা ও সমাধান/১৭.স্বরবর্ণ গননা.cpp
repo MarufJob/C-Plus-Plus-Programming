20d171a00b3e711a6188e341ea494f9045ca13eb
MD: Fazley Atif Maruf <106642848+MarufJob@users.noreply.github.com>
2024-05-25 01:52:20 +0600

    Update ১৭.স্বরবর্ণ গননা.cpp

--- ৫২ টি প্রোগ্রামিং সমস্যা ও সমাধান/১৭.স্বরবর্ণ গননা.cpp
+++ ৫২ টি প্রোগ্রামিং সমস্যা ও সমাধান/১৭.স্বরবর্ণ গননা.cpp
@@ -45,3 +45,57 @@ Output:Number of vowels = 6
 Input: Give a string: his last play was a big hit.
 Output:Number of vowels = 7
 */
+
+
+
+/*
+
+#include <iostream>
+//#include <cctype> //For tolower function
+using namespace std;
+
+int main()
+{
+    int i, T;
+    string str;
+
+    cout << "How many input are you taking = ";
+    cin >> T;
+    cin.ignore(); // Consume the newline character
+
+    for (i = 1; i <= T; i++)
+    {
+        cout << "Give a string: ";
+        getline(cin,str);
+        //cin.ignore(); // Consume the newline character after inputting the string
+
+        int count = 0;
+        for (char ch: str)
+        {
+            char lowerCh = tolower(ch);
+            if (lowerCh == 'a' || lowerCh == 'e' || lowerCh == 'i' || lowerCh == 'o' || lowerCh == 'u')
+            {
+                 count++;
+            }
+        }
+       cout <<"Number of vowels = "<<count<< endl;
+    }
+    return 0;
+}
+
+*/
+
+
+/*
+How many input are you taking = 3
+Give a string: FAZLEY ATIF MARUF
+Number of vowels = 6
+Give a string: fazley atif maruf
+Number of vowels = 6
+Give a string: happy coding
+Number of vowels = 3
+Give a string: HAPPY CODING
+Number of vowels = 3
+Give a string: I am a programmer
+Number of vowels = 6
+*/
