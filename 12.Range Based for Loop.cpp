c83b866068bb7c41e9ebe14abf42a9338c3652c1
MD: Fazley Atif Maruf <106642848+MarufJob@users.noreply.github.com>
2024-06-01 23:54:42 +0600

    Update 12.Range Based for Loop.cpp

--- 12.Range Based for Loop.cpp
+++ 12.Range Based for Loop.cpp
@@ -15,3 +15,59 @@ int main() {
 /*
 Output: 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 
 */
+
+
+
+/*
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main() {
+    
+    int t,i,j,S_len;
+    char str[25];
+    string S;
+
+
+    cout<<"Enter a string: ";
+    cin.getline(str,25);
+    // cin.get(S,10000);
+    // cin.ignore();// Consume the newline character after inputting the string
+    
+    S_len = strlen(str);
+    cout<<S_len<<endl;
+    
+    //for(j=0 ; S[j]!='\0' ; j++)
+    for(j=0 ; j<S_len ; j++)
+    {
+        cout<<str[j]<<" ";
+    }
+    
+    cout<<endl;
+    for(char c : str)
+    {
+        cout<<c<<" ";
+    }
+    cout<<endl;
+    
+    cout<<"Enter a string: ";
+    getline(cin,S);
+    
+    for(char c : S)
+    {
+        cout<<c<<" ";
+    }
+    return 0;
+}
+
+/*
+ Enter a string: Md. Fazley Atif Maruf
+21
+M d .   F a z l e y   A t i f   M a r u f 
+M d .   F a z l e y   A t i f   M a r u f . . . 0
+Enter a string: Md. Fazley Atif Maruf
+M d .   F a z l e y   A t i f   M a r u f 
+*/
+*/
