d86e4c23ff9c0f803b4e91f8612a6c3e66829124
MD: Fazley Atif Maruf <106642848+MarufJob@users.noreply.github.com>
2024-06-13 12:38:20 +0600

    Update ০৪.ভাজক.cpp

--- ৫২ টি প্রোগ্রামিং সমস্যা ও সমাধান/০৪.ভাজক.cpp
+++ ৫২ টি প্রোগ্রামিং সমস্যা ও সমাধান/০৪.ভাজক.cpp
@@ -37,3 +37,59 @@ Output :Case 2: 1 3 5 15
 Input :Give a number = 23
 Output :Case 3: 1 23
 /*
+
+
+
+/*
+#include <iostream>
+#include <cmath> // For sqrt function
+#include <vector> // For storing factors
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int T, num;
+
+    cout << "How many numbers are you taking = ";
+    cin >> T;
+
+    for (int i = 1; i <= T; ++i)
+    {
+        cout << "Give a number = ";
+        cin >> num;
+
+        cout << "Case " << i << ":";
+        
+        vector<int> factors;
+        int sqrtNum = sqrt(num);
+
+        // Find factors up to sqrt(num)
+        for (int j = 1; j <= sqrtNum; ++j)
+        {
+            if (num % j == 0)
+            {
+                factors.push_back(j);
+                if (j != num / j) // Avoid adding the square root twice if num is a perfect square
+                {
+                    factors.push_back(num / j);
+                }
+            }
+        }
+
+        // Sort the factors before printing
+        sort(factors.begin(), factors.end());
+
+        // Print factors
+        for (int factor : factors)
+        {
+            cout << " " << factor;
+        }
+        
+        cout << endl;
+    }
+
+    return 0;
+}
+*/
