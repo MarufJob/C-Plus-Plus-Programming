a0ec5ac7f18da2ebc876776474ed6b020ee7e8d6
MD: Fazley Atif Maruf <106642848+MarufJob@users.noreply.github.com>
2024-06-16 23:40:41 +0600

    Update ০৫.বক্স ১.cpp

--- ৫২ টি প্রোগ্রামিং সমস্যা ও সমাধান/০৫.বক্স ১.cpp
+++ ৫২ টি প্রোগ্রামিং সমস্যা ও সমাধান/০৫.বক্স ১.cpp
@@ -75,3 +75,35 @@ int main()
     return 0;
 }
 */
+
+
+
+/*
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i,j,k,T,num;
+
+    cout<<"How many number you taking = ";
+    cin>>T;
+
+    for(i=1 ; i<=T ; i++)
+    {
+        cout<<"Give a number = ";
+        cin>>num;
+
+        string row(num,'*');
+        
+        j=1;
+        while(j<=num)
+        {
+            //cout<<row<<endl;
+            cout << row << '\n';
+            j++;
+        }
+    }
+    return 0;
+}
+*/
