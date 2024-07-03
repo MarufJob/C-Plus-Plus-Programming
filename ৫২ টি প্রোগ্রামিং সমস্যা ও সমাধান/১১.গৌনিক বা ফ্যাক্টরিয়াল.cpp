6c59b8f3f46f90ed49f80185c4305909c2aafa8f
MD: Fazley Atif Maruf <106642848+MarufJob@users.noreply.github.com>
2024-07-03 23:34:16 +0600

    Update ১১.গৌনিক বা ফ্যাক্টরিয়াল.cpp

--- ৫২ টি প্রোগ্রামিং সমস্যা ও সমাধান/১১.গৌনিক বা ফ্যাক্টরিয়াল.cpp
+++ ৫২ টি প্রোগ্রামিং সমস্যা ও সমাধান/১১.গৌনিক বা ফ্যাক্টরিয়াল.cpp
@@ -1,32 +1,30 @@
 #include <iostream>
-
 using namespace std;
 
 int main()
 {
-    int T,i,j,nth;
-    long long int temp;
+    int i,j,nth,T;
+    long long int factotial;
 
     cout<<"How many number you taking = ";
     cin>>T;
 
     for(i=1 ; i<=T ; i++)
     {
-        temp=1;
+        factotial = 1;
         cout<<"Give a number = ";
         cin>>nth;
 
-        for(j=1 ; j<=nth ; j++)
+        for(j=1 ; j<=nth ; j++)    //or j=2
         {
-
-            temp = j*temp;
+            factotial  = j*factotial;
         }
-
-       cout<<temp<<endl;
+       cout<<factotial<<endl;
     }
-
     return 0;
 }
+
+
 
 /*
 How many number you taking = 3
@@ -36,4 +34,44 @@ Give a number = 10
 3628800
 Give a number = 15
 1307674368000
+*/
+
+
+
+/*
+#include <iostream>
+using namespace std;
+
+long long int factorial(int n);
+
+int main()
+{
+    int i,j,nth,T;
+    long long int result;
+    
+    cout<<"How many number you taking = ";
+    cin>>T;
+    
+    for(i=1 ; i<=T ; i++)
+    {
+        cout<<"Give a number = ";
+        cin>>nth;
+        
+        result = factorial(nth);
+        cout << "Factorial of " << nth << " = " << result << endl;
+    }
+    return 0;
+}
+
+long long int factorial(int n)
+{
+    if(n > 1)
+    {
+        return n * factorial(n-1);
+    }
+    else
+    {
+        return 1;
+    }
+}
 */
