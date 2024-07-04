cc0ca32f006f28ce3a5525d4e7a4e63c059b4be2
MD: Fazley Atif Maruf <106642848+MarufJob@users.noreply.github.com>
2024-07-04 21:12:56 +0600

    Update ১০.রান রেট-১.cpp

--- ৫২ টি প্রোগ্রামিং সমস্যা ও সমাধান/১০.রান রেট-১.cpp
+++ ৫২ টি প্রোগ্রামিং সমস্যা ও সমাধান/১০.রান রেট-১.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main()
 {
-    int i,T,num1, num2, num3, Odone;
+    int i,T,run1, run2, numBall, OvDone;
     float CurRunrate, NeedRunrate;
 
     cout<<"How many number you taking = ";
@@ -16,17 +16,19 @@ int main()
         NeedRunrate=0;
         
         cout<<"Give three number = ";
-        cin>>num1>>num2>>num3;
+        cin>>run1>>run2>>numBall;
         
-        Odone=300-num3;
-        CurRunrate=(num2*1.0/Odone)*6;
-        NeedRunrate=((num1*1.0-num2+1)/num3)*6;
+        OvDone=300-numBall;
+        CurRunrate=(run2*1.0/OvDone)*6;
+        NeedRunrate=((run1*1.0-run2+1)/numBall)*6;
         
         cout<<setprecision(3)<<CurRunrate<<" "<<setprecision(3)<<NeedRunrate<<endl;
     }
 
     return 0;
 }
+
+
 
 /*
 How many number you taking = 4
@@ -38,4 +40,34 @@ Input :Give three number = 333 250 40
 Output:5.77 12.6
 Input :Give three number = 118 100 180
 Output:5 0.633
+*/
+
+
+
+/*
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    int i,T,run1, run2, numBall, OvDone;
+    float CurRunrate, NeedRunrate;
+
+    cout<<"How many number you taking = ";
+    cin>>T;
+
+    for(i=1 ; i<=T ; i++)
+    {
+        cout<<"Give three number = ";
+        cin>>run1>>run2>>numBall;
+        
+        OvDone=300-numBall;
+        CurRunrate=(run2*6.0)/OvDone;
+        NeedRunrate=((run1-run2+1)*6.0)/numBall;
+        
+        cout<<setprecision(3)<<CurRunrate<<" "<<setprecision(3)<<NeedRunrate<<endl;
+    }
+    return 0;
+}
 */
