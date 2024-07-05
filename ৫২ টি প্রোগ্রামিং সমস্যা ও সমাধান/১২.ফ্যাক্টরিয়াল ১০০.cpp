3cbb7c03acac179d8b5111b0e3e758e2a4d62c99
MD: Fazley Atif Maruf <106642848+MarufJob@users.noreply.github.com>
2024-07-05 23:36:01 +0600

    Update ১২.ফ্যাক্টরিয়াল ১০০.cpp

--- ৫২ টি প্রোগ্রামিং সমস্যা ও সমাধান/১২.ফ্যাক্টরিয়াল ১০০.cpp
+++ ৫২ টি প্রোগ্রামিং সমস্যা ও সমাধান/১২.ফ্যাক্টরিয়াল ১০০.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main()
 {
-    int i,j,T,num,mod,count;
+    int i,j,T,count,mod,num;
     
     cout<<"How many number you taking = ";
     cin>>T;
@@ -31,11 +31,57 @@ int main()
 
 
 /*
+if(mod%5==0) -> 25!,50!,75!,100!
+
+5! = 5 = 1
+10! = 5 -> (5*2) = 2
+15! = 5 -> (5*2) -> (5*3) = 3
+20! = 5 -> (5*2) -> (5*3) -> (5*4) = 4
+25! = 5 -> (5*2) -> (5*3) -> (5*4) -> (5*5) = 6
+*/
+
+
+
+/*
 How many number you taking = 3
 Input :Give a number = 6
 Output:1
 Input :Give a number = 15
 Output:3
+Input :Give a number = 24
+Output:4
+Input :Give a number = 25
+Output:6
 Input :Give a number = 100
 Output:24
+*/
+
+
+
+/*
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i,T,count,num;
+    
+    cout<<"How many number you taking = ";
+    cin>>T;
+
+    for(i=1 ; i<=T ; i++)
+    {
+        count=0;
+        cout<<"Give a number = ";
+        cin>>num;
+
+        while(num>=5)
+        {
+            count+=num/5;
+            num=num/5;
+        }
+        cout<<count<<endl;
+    }
+    return 0;
+}
 */
