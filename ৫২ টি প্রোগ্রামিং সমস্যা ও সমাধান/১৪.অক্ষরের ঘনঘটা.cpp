f8fb060118029af66e4affd6bd6d59f95115a8e5
MD: Fazley Atif Maruf <106642848+MarufJob@users.noreply.github.com>
2024-07-08 15:19:04 +0600

    Update ১৪.অক্ষরের ঘনঘটা.cpp

--- ৫২ টি প্রোগ্রামিং সমস্যা ও সমাধান/১৪.অক্ষরের ঘনঘটা.cpp
+++ ৫২ টি প্রোগ্রামিং সমস্যা ও সমাধান/১৪.অক্ষরের ঘনঘটা.cpp
@@ -45,3 +45,57 @@ Input: Give a string: hello world
        a
 Output:'a' is not present
 */
+
+
+
+
+/*
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i, T;
+    string str;
+    char c;
+
+    cout << "How many input are you taking = ";
+    cin >> T;
+    cin.ignore(); // Consume the newline character
+
+    for (i = 1; i <= T; i++)
+    {
+        int num = 0;
+        cout << "Give a string: ";
+        getline(cin,str);
+        cin >> c;
+        cin.ignore();
+
+        for (char i : str)
+        {
+            if (i == c)
+            {
+                num++;
+            }
+            //cout<<i<<endl;
+        }
+        if(num>0)
+        cout <<"Occurrence of " <<"'"<<c<<"'"<<" in "<<"'"<<str<<"' = "<<num<< endl;
+        else
+        cout<<"'"<<c<<"'"<<" is not present"<<endl;
+    }
+    return 0;
+}
+*/
+
+
+
+/*
+How many input are you taking = 2
+Input: Give a string: hello world
+       l
+Output:Occurrence of 'l' in 'hello world' = 3
+Input: Give a string: hello world
+       a
+Output:'a' is not present
+*/
