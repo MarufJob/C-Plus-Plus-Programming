51f6d33efc74381ab6601cf47152e005a012b645
MD: Fazley Atif Maruf <106642848+MarufJob@users.noreply.github.com>
2024-07-27 07:05:11 +0600

    Add files via upload

---
+++ ৫২ টি প্রোগ্রামিং সমস্যা ও সমাধান/২৬.এলিয়েন গুপী.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i,T,num;
+
+    cout<<"How many number you taking = ";
+    cin>>T;
+
+    for(i=1 ; i<=T ; i++)
+    {
+        cout<<"Give a number = ";
+        cin>>num;
+
+        if(num%2==0)
+        {
+            cout<<"even"<<endl;
+        }
+        else
+        {
+            cout<<"odd"<<endl;
+        }
+
+        //(num%2==0)?cout<<"even"<<endl:cout<<"odd"<<endl;
+        //cout<<(num%2==0?"even":"odd")<<endl;
+    }
+
+    return 0;
+}
+
+
+
+/*
+How many number you taking = 3
+Input : Give a number = 100
+Output :even
+
+Input : Give a number = 0
+Output :even
+
+Input : Give a number = 1111
+Output :odd
+*/
