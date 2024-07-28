4dbb5532f82dccaa4c1750d29618bea4623525b6
MD: Fazley Atif Maruf <106642848+MarufJob@users.noreply.github.com>
2024-07-28 19:41:08 +0600

    Update ২৫.লঘিষ্ঠ সাধারণ গুণিতক.cpp

--- ৫২ টি প্রোগ্রামিং সমস্যা ও সমাধান/২৫.লঘিষ্ঠ সাধারণ গুণিতক.cpp
+++ ৫২ টি প্রোগ্রামিং সমস্যা ও সমাধান/২৫.লঘিষ্ঠ সাধারণ গুণিতক.cpp
@@ -1,45 +1,55 @@
 #include <iostream>
-
 using namespace std;
+
+int gcd(int a,int b)
+{
+    int temp;
+
+    while(b!=0)
+    {
+        temp=b;
+        b=a%b;
+        a=temp;
+    }
+    return a;
+}
+
 
 int main()
 {
-    int i,T,num;
+    int i,T,num1,num2,MUL,GCD,LCM;
 
     cout<<"How many number you taking = ";
     cin>>T;
 
     for(i=1 ; i<=T ; i++)
     {
-        cout<<"Give a number = ";
-        cin>>num;
+        cout<<"Give two number = ";
+        cin>>num1>>num2;
 
-        if(num%2==0)
-        {
-            cout<<"even"<<endl;
-        }
-        else
-        {
-            cout<<"odd"<<endl;
-        }
+        MUL=num1*num2;
 
-        //(num%2==0)?cout<<"even"<<endl:cout<<"odd"<<endl;
-        //cout<<(num%2==0?"even":"odd")<<endl;
+        GCD=gcd(num1,num2);
+
+        LCM=MUL/GCD;
+
+        cout<<"LCM = "<<LCM<<endl;
     }
-
     return 0;
 }
 
 
 
 /*
-How many number you taking = 3
-Input : Give a number = 100
-Output :even
-
-Input : Give a number = 0
-Output :even
-
-Input : Give a number = 1111
-Output :odd
+How many number you taking = 5
+Input : Give two number = 30 15
+Output: LCM = 30
+Input : Give two number = 12 16
+Output: LCM = 48
+Input : Give two number = 14 35
+Output: LCM = 70
+Input : Give two number = 4 6
+Output: LCM = 12
+Input : Give two number = 105 15
+Output: LCM = 105
 */
