36cef5f8a9f82ac117c5109c243f7eabb18eeadf
MD: Fazley Atif Maruf <106642848+MarufJob@users.noreply.github.com>
2024-07-31 21:58:27 +0600

    Update ২৮.এলোমেলো অ্যারে .cpp

--- ৫২ টি প্রোগ্রামিং সমস্যা ও সমাধান/২৮.এলোমেলো অ্যারে .cpp
+++ ৫২ টি প্রোগ্রামিং সমস্যা ও সমাধান/২৮.এলোমেলো অ্যারে .cpp
@@ -1,10 +1,9 @@
 #include <iostream>
-
 using namespace std;
 
 int main()
 {
-    int i,T,num;
+    int i,j,T,num;
 
     cout<<"How many number you taking = ";
     cin>>T;
@@ -14,32 +13,115 @@ int main()
         cout<<"Give a number = ";
         cin>>num;
 
-        if(num%2==0)
+        int array[num];
+
+        for(j=0 ; j<num ; j++)
         {
-            cout<<"even"<<endl;
+            cin>>array[j];
         }
+
+
+        int sorted = 1;
+
+
+        for(j=0 ; j<num ; j++)
+        {
+            if(array[j]<array[j+1])
+            {
+                sorted = 0;
+                break;
+            }
+        }
+
+        if(sorted)
+        cout<<"YES"<<endl;
+
         else
         {
-            cout<<"odd"<<endl;
+            sorted = 1;
+
+            for(j=0 ; j<num ; j++)
+            {
+                if(array[j]>array[j+1])
+                {
+                sorted = 0;
+                break;
+                }
+            }
+
+            if(sorted)
+            cout<<"YES"<<endl;
+            else
+            cout<<"No"<<endl;
         }
 
-        //(num%2==0)?cout<<"even"<<endl:cout<<"odd"<<endl;
-        //cout<<(num%2==0?"even":"odd")<<endl;
-    }
 
+
+
+        /*
+        for(j=0 ; j<num-1 ; j++)
+        {
+            if(array[j]-array[j+1]>=1)
+            {
+                cout<<"j1 = "<<j<<endl;
+                cout<<"array[j]-array[j+1] = "<<array[j]-array[j+1]<<endl;
+                cout<<"No"<<endl;
+                //break;
+            }
+
+            if(array[j]-array[j+1]==0)
+            {
+                cout<<"j3 = "<<j<<endl;
+                cout<<"array[j]-array[j+1] = "<<array[j]-array[j+1]<<endl;
+                cout<<"No"<<endl;
+                //break;
+            }
+            //cout<<array[j]<<" ";
+        }
+        //cout<<"Yes"<<endl;
+        */
+    }
     return 0;
 }
 
-
+//1 2 3 4 5
+//1 2 3 5 4
+//5 4 3 2 1
+//5 4 3 1 2
 
 /*
-How many number you taking = 3
-Input : Give a number = 100
-Output :even
+How many number you taking = 5
+Input : Give a number = 5
+        1 2 3 4 5
+Output :1 3 5
+Input : Give a number = 10
+        1 4 55 66 22 0 76 11 23 78
+Output :1 55 22 76 23
+Input : Give a number = 10
+        11 22 33 44 55 66 77 88 99 100
+Output :11 33 55 77 99
+Input : Give a number = 5
+        20722 501 27356 21714 91241
+Output :20722 27356 91241
+Input : Give a number = 3
+        51010 23119 73950
+Output :51010 73950
+*/
 
-Input : Give a number = 0
-Output :even
 
-Input : Give a number = 1111
-Output :odd
+
+/*            if(array[j]-array[j+1]>=1 || array[j]-array[j+1]<=-1 || array[j]-array[j+1]==0)
+
+        for(j=0 ; j<num-1 ; j++)
+        {
+            if(array[j]-array[j+1]!=1 || array[j]-array[j+1]!=1)
+            {
+                cout<<"j = "<<j<<endl;
+                cout<<"array[j]-array[j+1] = "<<array[j]-array[j+1]<<endl;
+                cout<<"No"<<endl;
+                //break;
+            }
+            //cout<<array[j]<<" ";
+        }
+        //cout<<"Yes"<<endl;
 */
