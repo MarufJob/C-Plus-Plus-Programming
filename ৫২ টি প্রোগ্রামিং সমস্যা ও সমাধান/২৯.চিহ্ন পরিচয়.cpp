7880341d7afcb2d038112d88c2fc201cce173b23
MD: Fazley Atif Maruf <106642848+MarufJob@users.noreply.github.com>
2024-08-01 21:32:21 +0600

    Update ২৯.চিহ্ন পরিচয়.cpp

--- ৫২ টি প্রোগ্রামিং সমস্যা ও সমাধান/২৯.চিহ্ন পরিচয়.cpp
+++ ৫২ টি প্রোগ্রামিং সমস্যা ও সমাধান/২৯.চিহ্ন পরিচয়.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
-
 using namespace std;
 
 int main()
 {
-    int i,T,num;
+    int i,T;
+    char ch;
 
     cout<<"How many number you taking = ";
     cin>>T;
@@ -12,19 +12,32 @@ int main()
     for(i=1 ; i<=T ; i++)
     {
         cout<<"Give a number = ";
-        cin>>num;
+        cin>>ch;
 
-        if(num%2==0)
+
+        if('A'<=ch && ch<='Z')
         {
-            cout<<"even"<<endl;
+            cout<<"Uppercase Character"<<endl;
         }
+
+        else if('a'<=ch && ch<='z')
+        {
+            cout<<"Lowercase Character"<<endl;
+        }
+
+        else if('0'<=ch && ch<='9')
+        {
+            cout<<"Numerical Digit"<<endl;
+        }
+
         else
         {
-            cout<<"odd"<<endl;
+            cout<<"Special Character"<<endl;
         }
 
-        //(num%2==0)?cout<<"even"<<endl:cout<<"odd"<<endl;
-        //cout<<(num%2==0?"even":"odd")<<endl;
+
+
+        //('A'<=ch && ch<='Z')?cout<<"Uppercase Character"<<endl:('a'<=ch && ch<='z')?cout<<"Lowercase Character"<<endl:('0'<=ch && ch<='9')?cout<<"Numerical Digit"<<endl:cout<<"Special Character"<<endl;
     }
 
     return 0;
@@ -33,13 +46,22 @@ int main()
 
 
 /*
-How many number you taking = 3
-Input : Give a number = 100
-Output :even
+How many number you taking = 7
+Input : Give a number = A
+Output: Uppercase Character
+Input : Give a number = M
+Output: Uppercase Character
 
-Input : Give a number = 0
-Output :even
+Input : Give a number = a
+Output: Lowercase Character
+Input : Give a number = m
+Output: Lowercase Character
 
-Input : Give a number = 1111
-Output :odd
+Input : Give a number = 5
+Output: Numerical Digit
+Input : Give a number = 9
+Output: Numerical Digit
+
+Input : Give a number = ;
+Output: Special Character
 */
