d8fd85382ae177675d51fb828b058d92c86f2555
MD: Fazley Atif Maruf <106642848+MarufJob@users.noreply.github.com>
2024-08-02 21:28:32 +0600

    Update ৩০.যোগ সংখ্যা ১.cpp

--- ৫২ টি প্রোগ্রামিং সমস্যা ও সমাধান/৩০.যোগ সংখ্যা ১.cpp
+++ ৫২ টি প্রোগ্রামিং সমস্যা ও সমাধান/৩০.যোগ সংখ্যা ১.cpp
@@ -4,7 +4,8 @@ using namespace std;
 
 int main()
 {
-    int i,T,num;
+    int i,j,T;//,num,sum;
+    unsigned long int num,sum;
 
     cout<<"How many number you taking = ";
     cin>>T;
@@ -14,32 +15,47 @@ int main()
         cout<<"Give a number = ";
         cin>>num;
 
-        if(num%2==0)
+        sum=0;
+
+        for(j=1; j<=num/2; j++)
         {
-            cout<<"even"<<endl;
+            if(num%j==0)
+            {
+                sum+=j;
+                //cout<<j<<" Sum = "<<sum<<endl;
+            }
+            //cout<<j<<" Sum = "<<sum<<endl;
+        }
+
+        if(num==sum)
+        {
+            cout<<"Yes, "<<num<<" is a perfect number!"<<endl;
         }
         else
         {
-            cout<<"odd"<<endl;
+            cout<<"No, "<<num<<" is not a perfect number!"<<endl;
         }
-
-        //(num%2==0)?cout<<"even"<<endl:cout<<"odd"<<endl;
-        //cout<<(num%2==0?"even":"odd")<<endl;
+        //cout<<i<<" = i"<<endl;
     }
-
     return 0;
 }
 
 
 
 /*
-How many number you taking = 3
-Input : Give a number = 100
-Output :even
-
-Input : Give a number = 0
-Output :even
-
-Input : Give a number = 1111
-Output :odd
+How many number you taking = 5
+Input : Give a number = 6
+Output: Yes, 6 is a perfect number!
+Input : Give a number = 28
+Output: Yes, 28 is a perfect number!
+Input : Give a number = 30
+Output: No, 30 is not a perfect number!
+Input : Give a number = 250
+Output: No, 250 is not a perfect number!
+Input : Give a number = 496
+Output: Yes, 496 is a perfect number!
+Input : Give a number = 8128
+Output: Yes, 8128 is a perfect number!
+Input : Give a number = 33550336
+Output: Yes, 33550336 is a perfect number!
 */
