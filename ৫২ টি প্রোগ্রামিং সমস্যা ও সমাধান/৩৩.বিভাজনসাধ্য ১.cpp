c33efe7d271d9abd9a0a8ffc9f6f23217f7f60a7
MD: Fazley Atif Maruf <106642848+MarufJob@users.noreply.github.com>
2024-08-04 15:45:15 +0600

    Update ৩৩.বিভাজনসাধ্য ১.cpp

--- ৫২ টি প্রোগ্রামিং সমস্যা ও সমাধান/৩৩.বিভাজনসাধ্য ১.cpp
+++ ৫২ টি প্রোগ্রামিং সমস্যা ও সমাধান/৩৩.বিভাজনসাধ্য ১.cpp
@@ -1,45 +1,59 @@
 #include <iostream>
-
 using namespace std;
 
 int main()
 {
-    int i,T,num;
+    int i,j,T,A,B,C,num;
 
     cout<<"How many number you taking = ";
     cin>>T;
 
     for(i=1 ; i<=T ; i++)
     {
-        cout<<"Give a number = ";
-        cin>>num;
+        cout<<"Give two number = ";
+        cin>>A>>B>>C;
 
-        if(num%2==0)
+        if(A<C)
         {
-            cout<<"even"<<endl;
-        }
-        else
-        {
-            cout<<"odd"<<endl;
+            A=C;
         }
 
-        //(num%2==0)?cout<<"even"<<endl:cout<<"odd"<<endl;
-        //cout<<(num%2==0?"even":"odd")<<endl;
+        else if(A>C && A%C!=0)
+        {
+            num=C-(A%C);
+            A+=num;
+            //cout<<A<<endl;
+        }
+
+        for(j=A; j<=B; j+=C)
+        {
+            cout<<j<<endl;
+        }
     }
-
     return 0;
 }
 
 
 
 /*
-How many number you taking = 3
-Input : Give a number = 100
-Output :even
-
-Input : Give a number = 0
-Output :even
-
-Input : Give a number = 1111
-Output :odd
+How many number you taking = 5
+Input : Give two number = 2 20 3
+Output: 3
+        6
+        9
+        12
+        15
+        18
+Input : Give two number = 50 60 5
+Output: 50
+        55
+        60
+Input : Give two number = 55 100 6
+Output: 60
+        66
+        72
+        78
+        84
+        90
+        96
 */
