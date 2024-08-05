4faf76f8462976efb0a5929bb3b6767b3ac2eafc
MD: Fazley Atif Maruf <106642848+MarufJob@users.noreply.github.com>
2024-08-05 19:50:16 +0600

    Update ৩৪.বিভাজনসাধ্য ২.cpp

--- ৫২ টি প্রোগ্রামিং সমস্যা ও সমাধান/৩৪.বিভাজনসাধ্য ২.cpp
+++ ৫২ টি প্রোগ্রামিং সমস্যা ও সমাধান/৩৪.বিভাজনসাধ্য ২.cpp
@@ -1,45 +1,47 @@
 #include <iostream>
-
 using namespace std;
 
 int main()
 {
-    int i,T,num;
+    int i,j,T;
+    long int A,B,C,num;
 
     cout<<"How many number you taking = ";
     cin>>T;
 
     for(i=1 ; i<=T ; i++)
     {
-        cout<<"Give a number = ";
-        cin>>num;
+        cout<<"Give three number = ";
+        cin>>A>>B>>C;
 
-        if(num%2==0)
-        {
-            cout<<"even"<<endl;
-        }
-        else
-        {
-            cout<<"odd"<<endl;
-        }
 
-        //(num%2==0)?cout<<"even"<<endl:cout<<"odd"<<endl;
-        //cout<<(num%2==0?"even":"odd")<<endl;
+        for(j=A*B; j<=C; j+=A*B)
+        {
+            cout<<j<<endl;
+        }
     }
-
     return 0;
 }
 
 
 
 /*
-How many number you taking = 3
-Input : Give a number = 100
-Output :even
-
-Input : Give a number = 0
-Output :even
-
-Input : Give a number = 1111
-Output :odd
+How many number you taking = 5
+Input : Give three number = 2 3 50
+Output: 6
+        12
+        18
+        24
+        30
+        36
+        42
+        48
+Input : Give three number = 3 5 50
+Output: 15
+        30
+        45
+Input : Give three number = 5 6 100
+Output: 30
+        60
+        90
 */
