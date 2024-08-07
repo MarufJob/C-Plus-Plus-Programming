5572696403a1cb3cfe1d5a77d541f9ffb1e196b0
MD: Fazley Atif Maruf <106642848+MarufJob@users.noreply.github.com>
2024-08-07 14:47:22 +0600

    Update ৩৮.হীরক রাজ্য.cpp

--- ৫২ টি প্রোগ্রামিং সমস্যা ও সমাধান/৩৮.হীরক রাজ্য.cpp
+++ ৫২ টি প্রোগ্রামিং সমস্যা ও সমাধান/৩৮.হীরক রাজ্য.cpp
@@ -1,45 +1,75 @@
 #include <iostream>
-
 using namespace std;
 
 int main()
 {
-    int i,T,num;
+    int i,j,k,l,num,T;
+    char ch;
 
     cout<<"How many number you taking = ";
     cin>>T;
+    cin.ignore(); // Consume the newline character
 
-    for(i=1 ; i<=T ; i++)
+    while(T--)
     {
-        cout<<"Give a number = ";
-        cin>>num;
+        cout<<"Give two number = ";
+        cin>>num>>ch;
+        //getline(cin,str);
 
-        if(num%2==0)
-        {
-            cout<<"even"<<endl;
-        }
-        else
-        {
-            cout<<"odd"<<endl;
-        }
 
-        //(num%2==0)?cout<<"even"<<endl:cout<<"odd"<<endl;
-        //cout<<(num%2==0?"even":"odd")<<endl;
+        for(i=1; i<=num; i++)
+        {
+            for(j=1; j<=i; j++)
+            {
+                cout<<ch<<" ";
+            }
+            cout<<endl;
+        }
+        
+        for(k=num-1; k>=1; k--)
+        {
+            for(l=k; l>=1; l--)
+            {
+                cout<<ch<<" ";
+            }
+            cout<<endl;
+        }
     }
-
     return 0;
 }
 
 
 
 /*
-How many number you taking = 3
-Input : Give a number = 100
-Output :even
-
-Input : Give a number = 0
-Output :even
-
-Input : Give a number = 1111
-Output :odd
+How many number you taking = 5
+Input : Give two number = 3 1
+Output: 1
+        1 1
+        1 1 1
+        1 1
+        1
+Input : Give two number = 5 2
+Output: 2
+        2 2
+        2 2 2
+        2 2 2 2
+        2 2 2 2 2
+        2 2 2 2
+        2 2 2
+        2 2
+        2
+Input : Give two number = 7 3
+Output: 3
+        3 3
+        3 3 3
+        3 3 3 3
+        3 3 3 3 3
+        3 3 3 3 3 3
+        3 3 3 3 3 3 3
+        3 3 3 3 3 3
+        3 3 3 3 3
+        3 3 3 3
+        3 3 3
+        3 3
+        3
 */
