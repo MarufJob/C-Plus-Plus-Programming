e0cacf5f9f9e9086289b321554d6d722b03df922
MD: Fazley Atif Maruf <106642848+MarufJob@users.noreply.github.com>
2024-08-09 22:08:28 +0600

    Update ৩৯.প্যালিনড্রোম.cpp

--- ৫২ টি প্রোগ্রামিং সমস্যা ও সমাধান/৩৯.প্যালিনড্রোম.cpp
+++ ৫২ টি প্রোগ্রামিং সমস্যা ও সমাধান/৩৯.প্যালিনড্রোম.cpp
@@ -47,3 +47,49 @@ Output: Sorry! It is not palindrome!
 Input : Give a number = civic
 Output: Yes! It is palindrome!
 */
+
+
+
+/*
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main()
+{
+    int i,j,leanth,T;
+
+    cout<<"How many number you taking = ";
+    cin>>T;
+    cin.ignore(); // Consume the newline character
+
+    while(T--)
+    {
+        cout<<"Give a number = ";
+        char str1[1000],str2[1000];
+        cin.get(str1, 1000);
+        cin.ignore(); // Consume the newline character after inputting the string
+
+        leanth=strlen(str1);
+        //cout<<leanth<<endl;
+
+        for(i=0,j=leanth-1; j>=0; i++,j--)
+        {
+            str2[i]=str1[j];
+            //cout<<str1[i]<<" = "<<str2[j]<<endl;
+        }
+        str2[i]='\0';
+        //cout<<str1<<" = "<<str2<<endl;
+
+        //bool num = strcmp(str1, str2);
+
+        //if(num == 0)
+        if(strcmp(str1, str2) == 0)
+        cout<<"Yes! It is palindrome!"<<endl;
+
+        else
+        cout<<"Sorry! It is not palindrome!"<<endl;
+    }
+    return 0;
+}
+*/
