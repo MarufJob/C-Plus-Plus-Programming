4e52fa33bf8e35c718706213738b1cfdb130b07f
MD: Fazley Atif Maruf <106642848+MarufJob@users.noreply.github.com>
2024-08-10 22:04:03 +0600

    Update ৪০.ধারার যোগফল ১.cpp

--- ৫২ টি প্রোগ্রামিং সমস্যা ও সমাধান/৪০.ধারার যোগফল ১.cpp
+++ ৫২ টি প্রোগ্রামিং সমস্যা ও সমাধান/৪০.ধারার যোগফল ১.cpp
@@ -45,3 +45,49 @@ Output: 12207031
 Input : Give a number = 10 5
 Output: 111111
 */
+
+
+
+/*
+How many number you taking = 5
+Input : Give a number = 5 2
+Output: 31
+Input : Give a number = 2 5
+Output: 63
+Input : Give a number = 2 10
+Output: 2047
+Input : Give a number = 5 10
+Output: 12207031
+Input : Give a number = 10 5
+Output: 111111
+*/
+
+
+
+/*
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i,j,T,num1,num2,sum,power;
+
+    cout<<"How many number you taking = ";
+    cin>>T;
+
+    for(i=1 ; i<=T ; i++)
+    {
+        cout<<"Give a number = ";
+        cin>>num1>>num2;
+
+        sum=1,power=1;
+        for(j=1; j<=num2; j++)
+        {
+            power*=num1;
+            sum+=power;
+        }
+        cout<<sum<<endl;
+    }
+    return 0;
+}
+*/
