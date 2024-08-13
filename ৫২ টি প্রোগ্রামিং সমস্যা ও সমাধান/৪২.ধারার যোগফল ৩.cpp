331c2fdf0275fd972cda0b02b1766286ebab5d94
MD: Fazley Atif Maruf <106642848+MarufJob@users.noreply.github.com>
2024-08-13 16:17:00 +0600

    Update ৪২.ধারার যোগফল ৩.cpp

--- ৫২ টি প্রোগ্রামিং সমস্যা ও সমাধান/৪২.ধারার যোগফল ৩.cpp
+++ ৫২ টি প্রোগ্রামিং সমস্যা ও সমাধান/৪২.ধারার যোগফল ৩.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
-
 using namespace std;
 
 int main()
 {
-    int i,T,num;
+    int i,j,T,num;
 
     cout<<"How many number you taking = ";
     cin>>T;
@@ -14,19 +13,20 @@ int main()
         cout<<"Give a number = ";
         cin>>num;
 
-        if(num%2==0)
+        for(j=num; j>=0; j--)
         {
-            cout<<"even"<<endl;
-        }
-        else
-        {
-            cout<<"odd"<<endl;
-        }
+            if(j>1)
+            cout<<"2^"<<j<<" + ";
+            else if(j==1)
+            cout<<"2 + ";
+            else
+            cout<<"1"<<endl;
 
-        //(num%2==0)?cout<<"even"<<endl:cout<<"odd"<<endl;
-        //cout<<(num%2==0?"even":"odd")<<endl;
+            //(j>1)?cout<<"2^"<<j<<" + ":(j==1)?cout<<"2 + ":cout<<"1"<<endl;
+            //cout<<(j>1?"2^" + to_string(j) + " + " : j == 1 ? "2 + " : "1");
+        }
+        //cout<<endl;
     }
-
     return 0;
 }
 
@@ -34,12 +34,10 @@ int main()
 
 /*
 How many number you taking = 3
-Input : Give a number = 100
-Output :even
-
-Input : Give a number = 0
-Output :even
-
-Input : Give a number = 1111
-Output :odd
+Input : Give a number = 5
+Output: 2^5 + 2^4 + 2^3 + 2^2 + 2 + 1
+Input : Give a number = 8
+Output: 2^8 + 2^7 + 2^6 + 2^5 + 2^4 + 2^3 + 2^2 + 2 + 1
+Input : Give a number = 10
+Output: 2^10 + 2^9 + 2^8 + 2^7 + 2^6 + 2^5 + 2^4 + 2^3 + 2^2 + 2 + 1
 */
