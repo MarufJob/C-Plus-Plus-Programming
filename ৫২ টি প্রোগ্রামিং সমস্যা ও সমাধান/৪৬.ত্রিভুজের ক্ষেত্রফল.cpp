52371d7dc469282380d1e11b1f5ff36e2a632ca7
MD: Fazley Atif Maruf <106642848+MarufJob@users.noreply.github.com>
2024-08-18 10:06:28 +0600

    Update ৪৬.ত্রিভুজের ক্ষেত্রফল.cpp

--- ৫২ টি প্রোগ্রামিং সমস্যা ও সমাধান/৪৬.ত্রিভুজের ক্ষেত্রফল.cpp
+++ ৫২ টি প্রোগ্রামিং সমস্যা ও সমাধান/৪৬.ত্রিভুজের ক্ষেত্রফল.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
-
+#include <math.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main()
 {
-    int i,T,num;
+    int i,T,num1,num2,num3;
+    double s,area;
 
     cout<<"How many number you taking = ";
     cin>>T;
@@ -12,34 +14,26 @@ int main()
     for(i=1 ; i<=T ; i++)
     {
         cout<<"Give a number = ";
-        cin>>num;
+        cin>>num1>>num2>>num3;
 
-        if(num%2==0)
-        {
-            cout<<"even"<<endl;
-        }
-        else
-        {
-            cout<<"odd"<<endl;
-        }
+        //s = 1.0*(num1+num2+num3)/2;
+        s = double(num1+num2+num3)/2;
+        area = sqrt(s*(s-num1)*(s-num2)*(s-num3));
 
-        //(num%2==0)?cout<<"even"<<endl:cout<<"odd"<<endl;
-        //cout<<(num%2==0?"even":"odd")<<endl;
+        cout<<"Area = "<< fixed <<setprecision(3)<<area<<endl;
     }
-
+    
     return 0;
 }
 
 
 
 /*
-How many number you taking = 3
-Input : Give a number = 100
-Output :even
-
-Input : Give a number = 0
-Output :even
-
-Input : Give a number = 1111
-Output :odd
+How many number you taking = 5
+Input : Give a number = 24 30 18
+Output: Area = 216.000
+Input : Give a number = 13 18 15
+Output: Area = 95.917
+Input : Give a number = 20 20 20
+Output: Area = 173.205
 */
