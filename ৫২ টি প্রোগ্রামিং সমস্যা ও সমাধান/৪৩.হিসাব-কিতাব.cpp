167a89e7549ff55723a0c5266df5d857bbc9f001
MD: Fazley Atif Maruf <106642848+MarufJob@users.noreply.github.com>
2024-08-22 11:01:27 +0600

    Update ৪৩.হিসাব-কিতাব.cpp

--- ৫২ টি প্রোগ্রামিং সমস্যা ও সমাধান/৪৩.হিসাব-কিতাব.cpp
+++ ৫২ টি প্রোগ্রামিং সমস্যা ও সমাধান/৪৩.হিসাব-কিতাব.cpp
@@ -30,9 +30,38 @@ int main()
 /*
 How many number you taking = 5
 Input : Give three number = 2 3 3
-Output: 2
+Output: Result = 2
 Input : Give three number = 2 10 5
-Output: 4
+Output: Result = 4
 Input : Give three number = 50 2 3
-Output: 1
+Output: Result = 1
+*/
+
+
+
+
+/*
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+    int i,j,k,T,num1,num2,num3,sum,power;
+
+    cout<<"How many number you taking = ";
+    cin>>T;
+
+    for(i=1 ; i<=T ; i++)
+    {
+        cout<<"Give three number = ";
+        cin>>num1>>num2>>num3;
+
+        sum=0;
+        power=pow(num1,num2);
+        sum+=power;
+        cout<<"Result = "<<sum%num3<<endl;
+    }
+    return 0;
+}
 */
