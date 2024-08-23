83b3ecc20fe404ca6bd339afc6e8fbd23d72f486
MD: Fazley Atif Maruf <106642848+MarufJob@users.noreply.github.com>
2024-08-23 12:04:11 +0600

    Update ৪৩.হিসাব-কিতাব.cpp

--- ৫২ টি প্রোগ্রামিং সমস্যা ও সমাধান/৪৩.হিসাব-কিতাব.cpp
+++ ৫২ টি প্রোগ্রামিং সমস্যা ও সমাধান/৪৩.হিসাব-কিতাব.cpp
@@ -20,7 +20,7 @@ int main()
             power*=num1;
         }
         sum+=power;
-        cout<<sum%num3<<endl;
+        cout<<"Result = "<<sum%num3<<endl;
     }
     return 0;
 }
@@ -61,6 +61,36 @@ int main()
         power=pow(num1,num2);
         sum+=power;
         cout<<"Result = "<<sum%num3<<endl;
+    }
+    return 0;
+}
+*/
+
+
+
+/*
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i,j,k,T,num1,num2,num3,power;
+
+    cout<<"How many number you taking = ";
+    cin>>T;
+
+    for(i=1 ; i<=T ; i++)
+    {
+        cout<<"Give three number = ";
+        cin>>num1>>num2>>num3;
+
+        power=1;
+
+        for(j=1; j<=num2; j++)
+        {
+            power=(num1*power)%num3;
+        }
+        cout<<"Result = "<<power%num3<<endl;
     }
     return 0;
 }
