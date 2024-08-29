a114214e17cd07a84dc755b446e0c37880efd505
MD: Fazley Atif Maruf <106642848+MarufJob@users.noreply.github.com>
2024-08-29 16:19:34 +0600

    Update ৩৫.বৃত্তের বাইরে.cpp

--- ৫২ টি প্রোগ্রামিং সমস্যা ও সমাধান/৩৫.বৃত্তের বাইরে.cpp
+++ ৫২ টি প্রোগ্রামিং সমস্যা ও সমাধান/৩৫.বৃত্তের বাইরে.cpp
@@ -1,44 +1,49 @@
 #include <iostream>
-
+#include <math.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main()
 {
-    int i,T,num;
+    int i,r,T,Xc,Yc,X1,Y1,XX,YY;
+    float mid_space;
 
     cout<<"How many number you taking = ";
     cin>>T;
 
     for(i=1 ; i<=T ; i++)
     {
-        cout<<"Give a number = ";
-        cin>>num;
+        cout<<"Give five number = ";
+        cin>>Xc>>Yc>>r>>X1>>Y1;
 
-        if(num%2==0)
+        XX=pow((X1-Xc),2);
+        YY=pow((Y1-Yc),2);
+        mid_space=sqrt(XX+YY);
+        //cout<<"result = "<< fixed <<setprecision(4)<<mid_space<<endl;
+
+        if(mid_space>r)
         {
-            cout<<"even"<<endl;
+            cout<<"The point is not inside the circle"<<endl;
         }
+
         else
         {
-            cout<<"odd"<<endl;
+            cout<<"The point is inside the circle"<<endl;
         }
-        //(num%2==0)?cout<<"even"<<endl:cout<<"odd"<<endl;
-        //cout<<(num%2==0?"even":"odd")<<endl;
     }
-
     return 0;
 }
 
 
 
 /*
-How many number you taking = 3
-Input : Give a number = 100
-Output :even
-
-Input : Give a number = 0
-Output :even
-
-Input : Give a number = 1111
-Output :odd
+How many number you taking = 2
+Input : Give five number = 1 1
+                           4
+                           10 -14
+Output: The point is not inside the circle
+Input : Give five number = 1 1
+                           8
+                           5 6
+Output: The point is inside the circle
 */
