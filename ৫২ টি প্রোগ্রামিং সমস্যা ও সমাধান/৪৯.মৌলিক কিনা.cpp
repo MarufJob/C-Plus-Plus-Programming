3a8424ff99b3cf4fbb76dc472ed43668870c72d1
MD: Fazley Atif Maruf <106642848+MarufJob@users.noreply.github.com>
2024-09-21 19:43:07 +0600

    Update ৪৯.মৌলিক কিনা.cpp

--- ৫২ টি প্রোগ্রামিং সমস্যা ও সমাধান/৪৯.মৌলিক কিনা.cpp
+++ ৫২ টি প্রোগ্রামিং সমস্যা ও সমাধান/৪৯.মৌলিক কিনা.cpp
@@ -1,45 +1,54 @@
 #include <iostream>
-
+#include <math.h>
 using namespace std;
 
-int main()
+int main() 
 {
-    int i,T,num;
-
-    cout<<"How many number you taking = ";
-    cin>>T;
-
-    for(i=1 ; i<=T ; i++)
+    int i,j,k,t,count,num1,num2,root;
+    
+    cout<<"How many input are you taking = ";
+    cin>>t;
+    cin.ignore();// Consume the newline character after inputting the string
+    
+    for(i=1 ; i<=t ; i++)
     {
-        cout<<"Give a number = ";
-        cin>>num;
+        count = 0;
 
-        if(num%2==0)
-        {
-            cout<<"even"<<endl;
-        }
-        else
-        {
-            cout<<"odd"<<endl;
-        }
+        cout<<"Number = ";
+        cin>>num1;
+        //cout<<"end = ";
+        //cin>>num2;
 
-        //(num%2==0)?cout<<"even"<<endl:cout<<"odd"<<endl;
-        //cout<<(num%2==0?"even":"odd")<<endl;
+
+        //for(k=num1 ; k<=num2 ; k++)
+        //{
+            int countNum = 0;//loop_iteration = 0;
+            root = sqrt(num1);
+    
+            //for(j=2 ; j<=k/j ; j++)
+            //for(j=2 ; j*j<=k ; j++)
+            for(j=2 ; j<=root ; j++)
+            {
+                if(num1%j==0)
+                {
+                    countNum = 1;
+                    break;
+                }
+                //loop_iteration++;
+            }
+            
+            cout<<countNum<<endl;
+            if(countNum == 0) //&& k >1)
+            cout<<num1<<" is a prime"<<endl;
+                //count++;
+                //cout<<k<<" -> "<<loop_iteration<<endl;
+                //cout<<k<<" ";
+            else
+            cout<<num1<<" is not a prime"<<endl;
+        //}
+        //cout<<"T = "<<t<<"  "<<count<<endl;
+        //cout<<count<<endl;
+        //cout<<endl;
     }
-
     return 0;
 }
-
-
-
-/*
-How many number you taking = 3
-Input : Give a number = 100
-Output :even
-
-Input : Give a number = 0
-Output :even
-
-Input : Give a number = 1111
-Output :odd
-*/
