ed6caab3e5f320fcc76c735dd54d85fccd9e317b
MD: Fazley Atif Maruf <106642848+MarufJob@users.noreply.github.com>
2024-09-29 00:13:25 +0600

    Update ৪৯.মৌলিক কিনা.cpp

--- ৫২ টি প্রোগ্রামিং সমস্যা ও সমাধান/৪৯.মৌলিক কিনা.cpp
+++ ৫২ টি প্রোগ্রামিং সমস্যা ও সমাধান/৪৯.মৌলিক কিনা.cpp
@@ -328,3 +328,115 @@ int main() {
     }
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+//#define size 1000001
+//int array [size];
+
+
+
+/*
+void sieve()
+{
+    int i,j,root;
+
+     for(i=2; i<size; i++)
+    {
+        array[i]=1;
+    }
+
+    root = sqrt(size);
+
+    for(i=2; i<=root; i++)
+    {
+        if(array[i]==1)
+        {
+             for(j=2; i*j<=size; j++)
+            {
+                array[i * j] = 0;
+            }
+        }
+    }
+
+}
+*/
+
+
+
+void find_prime_number(int prime_are[], int n)
+{
+    int i,j,root;
+    
+    int size = 1000000
+    root = sqrt(size);
+
+    for(i=2; i<size; i++)
+    {
+        prime_are[i]=1;
+    }
+
+    
+
+    for(i=2; i<=root; i++)
+    {
+        prime_are[i]=i;
+        if(prime_are[i]==1)
+        {
+            //prime_are[i]=i;
+            for(j=2; i*j<=size; j++)
+            {
+                prime_are[i * j] = 0;
+            }
+        }
+    }
+    
+    for(i=2; i<size; i++)
+    {
+        cout<<prime_are[i]<<" -- ";
+    }
+}
+
+
+
+int main() {
+    int prime_ara[1000000];
+    int i,T,sqrt_n;
+    long long int N;
+    
+    cin>>T;
+    
+    while(T--)
+    {
+        cin>>N;
+        sqrt_n = sqrt(N);
+        find_prime_number(prime_ara);
+        int is_prime = 1;
+        
+        for(i = 0; prime_ara[i] <= sqrt_n; i++)
+        {
+            if(N % prime_ara[i] == 0)
+            {
+                is_prime = 0;
+                break;
+            }
+        }
+        
+        if(is_prime == 1)
+        cout<<N<<" is a prime"<<endl;
+        
+        else
+        cout<<N<<" is not a prime"<<endl;
+    }
+    return 0;
+}
