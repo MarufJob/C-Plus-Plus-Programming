b427e12b0bbd33af01d6d1ac36ab698f4b752492
MD: Fazley Atif Maruf <106642848+MarufJob@users.noreply.github.com>
2024-10-04 14:29:13 +0600

    Update 05.Prime or Non-Prime number.cpp

--- Logic Building/05.Prime or Non-Prime number.cpp
+++ Logic Building/05.Prime or Non-Prime number.cpp
@@ -28,6 +28,8 @@ int main() {
    
 }
 
+
+
 /*
 Input: Give a number = 53
 Output: 53 is Prime
@@ -48,3 +50,80 @@ Output: 21 is Nonprime
    
    If the number is greater than or equal to 2, the program calculates the square root of n (stored in the variable m). This is an optimization because, in the process of checking for divisors, you only need to check up to the square root of n.
 */
+
+
+
+
+#include <stdio.h>   
+#include <math.h>
+ int size = 50;   
+ int ara[50];   
+
+ void print_ara()   
+ {   
+     int i;   
+     for(i = 2; i < size; i++) {   
+         printf("%4d", ara[i]);   
+     }   
+     printf("\n");   
+     for(i = 2; i < size; i++) {   
+         printf("----");   
+     }   
+     printf("\n");   
+     for(i = 2; i < size; i++) {   
+         printf("%4d", i);   
+     }   
+     printf("\n\n\n");    
+ }   
+
+ void sieve()   
+ {   
+     int i, j, root;   
+     for(i = 2; i < size; i++) {   
+         ara[i] = 1;   
+     }   
+     root = sqrt(size);   
+     print_ara();   
+     for(i = 2; i <= root; i++) {      
+         if(ara[i] == 1) {   
+             for(j = 2; i * j <= size; j++) {   
+                 ara[i * j] = 0;          
+             }
+             printf("i = %4d\n", i);
+             print_ara();   
+         }   
+     }   
+ }   
+
+ int is_prime(int n)   
+ {   
+     int i;   
+     if(n < 2) {   
+         return 0;   
+     }   
+     return ara[n];   
+ }   
+
+ int main()   
+ {    
+     int n, m;   
+     sieve();   
+     while(1) {   
+         printf("Please enter a number (enter 0 to exit): ");   
+         scanf("%d", &n);   
+         if(n == 0) {   
+             break;   
+         }   
+         if(n >= size) {   
+             printf("The number should be less than %d\n", size);   
+             continue;   
+         }   
+         if(1 == is_prime(n)) {   
+             printf("%d is a prime number.\n", n);   
+         }   
+         else {   
+             printf("%d is not a prime number.\n", n);   
+         }   
+     }      
+     return 0;   
+ }
