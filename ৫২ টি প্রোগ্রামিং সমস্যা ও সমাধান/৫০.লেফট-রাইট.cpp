4d80223a05d485b137947971c58a9d44cc4c6775
MD: Fazley Atif Maruf <106642848+MarufJob@users.noreply.github.com>
2024-10-07 21:56:23 +0600

    Update ৫০.লেফট-রাইট.cpp

--- ৫২ টি প্রোগ্রামিং সমস্যা ও সমাধান/৫০.লেফট-রাইট.cpp
+++ ৫২ টি প্রোগ্রামিং সমস্যা ও সমাধান/৫০.লেফট-রাইট.cpp
@@ -1,45 +1,65 @@
 #include <iostream>
-
+#include <cstring>
 using namespace std;
 
-int main()
-{
-    int i,T,num;
-
-    cout<<"How many number you taking = ";
-    cin>>T;
-
-    for(i=1 ; i<=T ; i++)
+int main() {
+    
+    int t,i,j,S_len;
+    char str[50];
+    
+    cout<<"How many input are you taking = ";
+    cin>>t;
+    cin.ignore();// Consume the newline character after inputting the string
+    
+    for(i=0 ; i<t ; i++)
     {
-        cout<<"Give a number = ";
-        cin>>num;
-
-        if(num%2==0)
+        cout<<"Enter a string: ";
+        
+        //cin.getline(str,50);
+        cin.get(str,50);
+        cin.ignore();// Consume the newline character after inputting the string
+        
+        
+        S_len = strlen(str);
+        // cout<<S_len<<endl;
+        // cout<<sizeof(S)/sizeof(S[0])<<endl;
+        
+        //for(j=0 ; S[j]!='\0' ; j++)
+        for(j=0 ; j<S_len ; j++)
         {
-            cout<<"even"<<endl;
+            if(str[j]=='L')
+            {
+                // cout<<str[j]<<endl;
+                // cout<<str[j-1]<<endl;
+                str[j]=str[j-1];
+                // cout<<str[j]<<endl;
+            }
+            
+            if(str[j]=='R')
+            {
+                // cout<<str[j]<<endl;
+                // cout<<str[j+1]<<endl;
+                str[j]=str[j+1];
+                // cout<<str[j]<<endl;
+            }
         }
-        else
-        {
-            cout<<"odd"<<endl;
-        }
-
-        //(num%2==0)?cout<<"even"<<endl:cout<<"odd"<<endl;
-        //cout<<(num%2==0?"even":"odd")<<endl;
-    }
-
+        cout<<str<<endl;
+     }
     return 0;
 }
 
 
 
 /*
-How many number you taking = 3
-Input : Give a number = 100
-Output :even
-
-Input : Give a number = 0
-Output :even
-
-Input : Give a number = 1111
-Output :odd
+How many input are you taking = 5
+Input :Enter a string: 0L7
+Output:007
+Input :Enter a string: 4R5L9
+Output:45559
+Input :Enter a string: 71
+Output:71
+Input :Enter a string: 8R4R0
+Output:84400
+Input :Enter a string: 34R92L6
+Output:3499226
 */
