4f606e78d545b3285cffd55e6205bb88ce6c65a5
MD: Fazley Atif Maruf <106642848+MarufJob@users.noreply.github.com>
2024-10-09 10:33:57 +0600

    Update ৫১.খোঁজ দ্য সার্চ ১.cpp

--- ৫২ টি প্রোগ্রামিং সমস্যা ও সমাধান/৫১.খোঁজ দ্য সার্চ ১.cpp
+++ ৫২ টি প্রোগ্রামিং সমস্যা ও সমাধান/৫১.খোঁজ দ্য সার্চ ১.cpp
@@ -78,3 +78,62 @@ Input :Enter a string and substring: fazley_atif_maruf
                                      maruf
 Output:12
 */
+
+
+
+/*
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main() {
+    
+    int t,i,j,k,S1_len,S2_len;
+    char str1[128],str2[128];
+    
+    cout<<"How many input are you taking = ";
+    cin>>t;
+    cin.ignore();// Consume the newline character after inputting the string
+    
+    for(i=0 ; i<t ; i++)
+    {
+        cout<<"Enter a string and substring: ";
+        //cin.getline(str,50);
+        cin.get(str1,128);
+        cin.ignore();// Consume the newline character after inputting the string
+        
+        //cin.getline(str,50);
+        cin.get(str2,128);
+        cin.ignore();// Consume the newline character after inputting the string
+        
+        S1_len = strlen(str1);
+        S2_len = strlen(str2);
+        // cout<<"S2_len = "<<S2_len<<endl;
+        // cout<<sizeof(str2)/sizeof(str2[0])<<endl;
+        
+        //for(j=0 ; S[j]!='\0' ; j++)
+        for(j=0 ; j<S1_len ; j++)
+        {
+            if(str1[j]==str2[0])
+            {
+                // cout<<str1[j]<<" = "<<j<<endl;
+                for(k=1 ; k<S2_len ; k++)
+                {
+                    if(str2[k]!=str1[j+k])
+                    {
+                        break;
+                    }
+                    
+                }
+                if(k==S2_len)
+                {
+                    // cout<<"Index = "<<j<<endl;
+                    cout<<j<<endl;
+                    break;
+                }
+            }
+        }
+     }
+    return 0;
+}
+*/
