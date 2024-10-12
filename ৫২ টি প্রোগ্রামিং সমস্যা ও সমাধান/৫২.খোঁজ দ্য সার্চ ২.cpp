2d25290063b99faf263e72ca3c22f8d21b2ce00d
MD: Fazley Atif Maruf <106642848+MarufJob@users.noreply.github.com>
2024-10-12 20:32:53 +0600

    Update ৫২.খোঁজ দ্য সার্চ ২.cpp

--- ৫২ টি প্রোগ্রামিং সমস্যা ও সমাধান/৫২.খোঁজ দ্য সার্চ ২.cpp
+++ ৫২ টি প্রোগ্রামিং সমস্যা ও সমাধান/৫২.খোঁজ দ্য সার্চ ২.cpp
@@ -1,45 +1,81 @@
 #include <iostream>
-
+#include <cstring>
 using namespace std;
 
-int main()
-{
-    int i,T,num;
-
-    cout<<"How many number you taking = ";
-    cin>>T;
-
-    for(i=1 ; i<=T ; i++)
+int main() {
+    
+    int t,i,j,k,S1_len,S2_len;
+    char str1[128],str2[128];
+    
+    cout<<"How many input are you taking = ";
+    cin>>t;
+    cin.ignore();// Consume the newline character after inputting the string
+    
+    for(i=0 ; i<t ; i++)
     {
-        cout<<"Give a number = ";
-        cin>>num;
-
-        if(num%2==0)
+        cout<<"Enter a string and substring: ";
+        //cin.getline(str,50);
+        cin.get(str1,128);
+        cin.ignore();// Consume the newline character after inputting the string
+        
+        //cin.getline(str,50);
+        cin.get(str2,128);
+        cin.ignore();// Consume the newline character after inputting the string
+        
+        int count=0;
+        
+        S1_len = strlen(str1);
+        S2_len = strlen(str2);
+        // cout<<"S2_len = "<<S2_len<<endl;
+        // cout<<sizeof(str2)/sizeof(str2[0])<<endl;
+        
+        //for(j=0 ; S[j]!='\0' ; j++)
+        for(j=0 ; j<S1_len ; j++)
         {
-            cout<<"even"<<endl;
+            if(str1[j]==str2[0])
+            {
+                // cout<<str1[j]<<" = "<<j<<endl;
+                for(k=1 ; k<S2_len ; k++)
+                {
+                    if(str2[k]!=str1[j+k])
+                    {
+                        // count=1;
+                        break;
+                    }
+                    
+                
+                    
+                }
+                if(k==S2_len)
+                {
+                    //cout<<"K = "<<k<<" "<<"S2_len = "<<S2_len<<endl;
+                    count++;
+                    //cout<<count<<endl;
+                }
+            }
         }
-        else
-        {
-            cout<<"odd"<<endl;
-        }
-
-        //(num%2==0)?cout<<"even"<<endl:cout<<"odd"<<endl;
-        //cout<<(num%2==0?"even":"odd")<<endl;
-    }
-
+        cout<<count<<endl;
+     }
     return 0;
 }
 
 
 
 /*
-How many number you taking = 3
-Input : Give a number = 100
-Output :even
-
-Input : Give a number = 0
-Output :even
-
-Input : Give a number = 1111
-Output :odd
+How many input are you taking = 5
+Input :Enter a string and substring: banana
+                                     ana
+Output:2
+Input :Enter a string and substring: banana
+                                     anna
+Output:0
+Input :Enter a string and substring: aquickbrownfoxjumpsoverthelazydog
+                                     fox
+Output:1
+Input :Enter a string and substring: ddddd
+                                     ddd
+Output:3
+Input :Enter a string and substring: foobar
+                                     foobar
+Output:1
 */
