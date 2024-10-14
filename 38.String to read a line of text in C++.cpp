c0df902e682bed6e349745e607df20aadc6f0280
MD: Fazley Atif Maruf <106642848+MarufJob@users.noreply.github.com>
2024-10-14 11:45:43 +0600

    Update 38.String to read a line of text in C++.cpp

--- 38.String to read a line of text in C++.cpp
+++ 38.String to read a line of text in C++.cpp
@@ -6,6 +6,8 @@ int main()
     char str[100];
     cout << "Enter a string: ";
     cin.get(str, 100);
+    // cin.ignore();// Consume the newline character after inputting the string
+    // cin.getline(str,100);
 
     cout << "You entered: " << str << endl;
     return 0;
@@ -20,4 +22,10 @@ To read the text containing blank space, cin.get function can be used. This func
 First argument is the name of the string (address of first element of string) and second argument is the maximum size of the array.
 
 In the above program, str is the name of the string and 100 is the maximum size of the array.
+*/
+
+
+
+/*
+cin.getline(str, 100);: এখানে cin.getline() ব্যবহার করা হয়েছে যা ইনপুট নেয়ার পরে নিউলাইন ক্যারেক্টার নিজে থেকেই বাদ দিয়ে দেয়, তাই আর cin.ignore() দরকার নেই।
 */
