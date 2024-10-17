d1a4f5ce96344c3b5852b4e2f384e980c04b842e
MD: Fazley Atif Maruf <106642848+MarufJob@users.noreply.github.com>
2024-10-17 12:23:17 +0600

    Update ৫১.খোঁজ দ্য সার্চ ১.cpp

--- ৫২ টি প্রোগ্রামিং সমস্যা ও সমাধান/৫১.খোঁজ দ্য সার্চ ১.cpp
+++ ৫২ টি প্রোগ্রামিং সমস্যা ও সমাধান/৫১.খোঁজ দ্য সার্চ ১.cpp
@@ -137,3 +137,73 @@ int main() {
     return 0;
 }
 */
+
+
+
+/*
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main() {
+    
+    int t,i,j,k,S1_len,S2_len;
+    char str1[128],str2[128];
+    
+    cout<<"How many input are you taking = ";
+    cin>>t;
+    cin.ignore();// Consume the newline character after inputting the string
+    
+    for(i=0 ; i<t ; i++)
+    {
+        cout<<"Enter a string and substring: ";
+        cin>>str1>>str2;
+        
+        S1_len = strlen(str1);
+        S2_len = strlen(str2);
+        // cout<<"S2_len = "<<S2_len<<endl;
+        // cout<<sizeof(str2)/sizeof(str2[0])<<endl;
+        
+        //for(j=0 ; S[j]!='\0' ; j++)
+        for(j=0 ; j<S1_len ; j++)
+        {
+            if(str1[j]==str2[0])
+            {
+                // cout<<str1[j]<<" = "<<j<<endl;
+                for(k=1 ; k<S2_len ; k++)
+                {
+                    if(str2[k]!=str1[j+k])
+                    {
+                        break;
+                    }
+                    
+                }
+                if(k==S2_len)
+                {
+                    // cout<<"Index = "<<j<<endl;
+                    cout<<j<<endl;
+                    break;
+                }
+            }
+        }
+     }
+    return 0;
+}
+*/
+
+
+
+
+/*
+How many input are you taking = 5
+Input :Enter a string and substring: banana ana
+Output:1
+Input :Enter a string and substring: banana ban
+Output:0
+Input :Enter a string and substring: aquickbrownfoxjumpsoverthelazydog fox
+Output:11
+Input :Enter a string and substring: foobar foobar
+Output:0
+Input :Enter a string and substring: fazley_atif_maruf maruf
+Output:12
+*/
